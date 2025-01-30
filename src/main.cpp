b75f3d7bce7248df166d806b54372657d3402e7c
William Norland <williamnorland@gmail.com>
2025-01-30 14:00:37 +0100

    Add dummy main for debug purposes

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "main.h"
+
+/**
+ * @brief main function
+ * 
+ * @return 0
+ */
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
+
+/**
+ * @brief funny function
+ * 
+ * @return a funny number
+ */
+int fun1() {
+    return 1;
+}
