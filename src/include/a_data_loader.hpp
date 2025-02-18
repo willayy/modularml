ef2d78f9a81e5cf1d07ed65670fa0eaba52686df
Tim <timcarlsson0203@gmail.com>
2025-02-18 15:32:14 +0100

    Add documentation

--- src/include/a_data_loader.hpp
+++ src/include/a_data_loader.hpp
@@ -2,9 +2,22 @@
 
 #include <string>
 
+
+/**
+ * @class DataLoader
+ * @brief Abstract class for loading external files.
+ */
 class DataLoader
 {  
   public:
+    /**
+     * @brief Loads a file and writes the output to the destination path.
+     * 
+     * @param onnx_path Path to the file that is to be read.abs
+     * @param destination_path Path to the where the result is written.
+     */
     virtual void load(std::string& onnx_path, std::string& destination_path) const = 0;
+    
+    /// @brief Virtual destructor for cleanup.
     virtual ~DataLoader() = default;
 };
