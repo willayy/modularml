3db36b196fdca20ba67227f3331836ea5060adb0
Måns Bremer <mans.bremer@gmail.com>
2025-02-20 16:39:48 +0100

    remove unnecessary virtual specifier

--- src/include/a_tensor_func.hpp
+++ src/include/a_tensor_func.hpp
@@ -20,7 +20,7 @@ public:
 
     /// @brief Virtual destructor for TensorFunction.
     /// @details Ensures derived class destructors are called properly.
-    virtual ~TensorFunction() = default;
+    ~TensorFunction() = default;
 
     // To be implemented by derived classes:
 
