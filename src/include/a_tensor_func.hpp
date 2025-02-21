3d524691d00ac9fbbefa82a23e742e1bd23489f5
Måns Bremer <mans.bremer@gmail.com>
2025-02-21 14:10:11 +0100

    Added virtual back

--- src/include/a_tensor_func.hpp
+++ src/include/a_tensor_func.hpp
@@ -20,7 +20,7 @@ public:
 
     /// @brief Virtual destructor for TensorFunction.
     /// @details Ensures derived class destructors are called properly.
-    ~TensorFunction() = default;
+    virtual ~TensorFunction() = default;
 
     // To be implemented by derived classes:
 
