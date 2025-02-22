637062b79c32c95773772dc29ad45e1a52ac7c53
William Norland <williamnorland@gmail.com>
2025-02-22 17:18:47 +0100

    Create factory functions for tensors using standard arith and datastructures

---
+++ src/include/mml_tensors.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "globals.hpp"
+#include "mml_arithmetic.hpp"
+#include "mml_vector_ds.hpp"
+#include "tensor.hpp"
+
+Tensor<float> tensor_mll(vec<int> shape);
+
+Tensor<float> tensor_mll(vec<int> shape, vec<float> &data);
+
+Tensor<float> tensor_mll(const vec<int> shape, const vec<float> data);
---
+++ src/mml_tensors.cpp
@@ -0,0 +1,30 @@
+#include "mml_tensors.hpp"
+
+/// @brief Initializes a new tensor with the given shape and all elements set to zero.
+/// @param shape The shape of the tensor.
+/// @return A new tensor with the given shape and all elements set to zero.
+Tensor<float> tensor_mll(const vec<int> shape) {  // NOSONAR - function signature is correct
+  const auto ds = new Vector_mml<float>(shape);
+  const auto am = new Arithmetic_mml();
+  return Tensor<float>(ds, am);
+}
+
+/// @brief Initializes a new tensor with the given shape and data.
+/// @param shape The shape of the tensor.
+/// @param data A reference to the data to be set in the tensor.
+/// @return A new tensor with the given shape and data.
+Tensor<float> tensor_mll(const vec<int> shape, const vec<float>& data) {  // NOSONAR - function signature is correct
+  const auto ds = new Vector_mml<float>(shape, data);
+  const auto am = new Arithmetic_mml();
+  return Tensor<float>(ds, am);
+}
+
+/// @brief Initializes a new tensor with the given shape and data.
+/// @param shape The shape of the tensor.
+/// @param data The data to be set in the tensor.
+/// @return A new tensor with the given shape and data.
+Tensor<float> tensor_mll(const vec<int> shape, const vec<float> data) { // NOSONAR - function signature is correct
+  const auto ds = new Vector_mml<float>(shape, data);
+  const auto am = new Arithmetic_mml();
+  return Tensor<float>(ds, am);
+}
