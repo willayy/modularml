8c55ab2013d7923c0b26717b62f90824e527fe3a
William Norland <williamnorland@gmail.com>
2025-02-22 17:04:17 +0100

    Remove old tests

--- tests/test1.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <cassert>
-
-int main() {
-    static_assert(2 + 2 == 4, "Test 1!");
-}
--- tests/test2.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <cassert>
-
-int main() {
-    static_assert(2 + 3 == 5, "Test 2!");
-}
