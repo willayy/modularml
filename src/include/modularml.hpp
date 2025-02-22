a15ca5540de795f232f28cca49ca50cd7177c007
William Norland <williamnorland@gmail.com>
2025-02-22 17:45:02 +0100

    Create header file for the whole library

---
+++ src/include/modularml.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "a_arithmetic_module.hpp"
+#include "a_data_structure.hpp"
+#include "a_tensor_func.hpp"
+#include "globals.hpp"
+#include "mml_arithmetic.hpp"
+#include "mml_vector_ds.hpp"
+#include "mml_tensors.hpp"
+#include "tensor.hpp"
