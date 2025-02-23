39e360fc62f899a572fc443a25dbef69251fbdee
William Norland <williamnorland@gmail.com>
2025-02-23 22:14:04 +0100

    Refactor and fix mml arithmetic module

--- src/include/mml_arithmetic.hpp
+++ src/include/mml_arithmetic.hpp
@@ -5,47 +5,75 @@
 
 class Arithmetic_mml : public ArithmeticModule<float> {
  public:
-
   ~Arithmetic_mml() override = default;
 
-  DataStructure<float>* add(DataStructure<float>* a, const DataStructure<float>* b) const override {
-    for (int i = 0; i < a->get_size(); i++) {
-      vec<int> ind = vec<int>{i};
-      a->set(ind, a->get(ind) + b->get(ind));
+  DataStructure<float>* add(const DataStructure<float>* a, const DataStructure<float>* b) const override {
+    const auto& a_raw = a->get_raw_data();
+    const auto& b_raw = b->get_raw_data();
+    auto res_raw = vec<float>(a->get_data_size(), 0);
+    for (int i = 0; i < a->get_data_size(); i++) {
+      res_raw[i] = a_raw[i] + b_raw[i];
     }
-    return a;
+    Vector_mml<float>* res = new Vector_mml<float>(a->get_shape(), res_raw);
+    return res;
   }
 
-  DataStructure<float>* subtract(DataStructure<float>* a, const DataStructure<float>* b) const override {
-    for (int i = 0; i < a->get_size(); i++) {
-      vec<int> ind = vec<int>{i};
-      a->set(ind, a->get(ind) - b->get(ind));
+  DataStructure<float>* subtract(const DataStructure<float>* a, const DataStructure<float>* b) const override {
+    const auto& a_raw = a->get_raw_data();
+    const auto& b_raw = b->get_raw_data();
+    auto res_raw = vec<float>(a->get_data_size(), 0);
+    for (int i = 0; i < a->get_data_size(); i++) {
+      res_raw[i] = a_raw[i] - b_raw[i];
     }
-    return a;
+    Vector_mml<float>* res = new Vector_mml<float>(a->get_shape(), res_raw);
+    return res;
   }
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-  DataStructure<float>* multiply(DataStructure<float>* a, const DataStructure<float>* b) const override {
-    return nullptr; // TODO: Implement this
+  DataStructure<float>* multiply(const DataStructure<float>* a, const DataStructure<float>* b) const override {
+    return nullptr;  // TODO: Implement this
   }
 
-  DataStructure<float>* multiply(DataStructure<float>* a, const float b) const override {
-    for (int i = 0; i < a->get_size(); i++) {
-      vec<int> ind = vec<int>{i};
-      a->set(ind, a->get(ind) * b);
+  DataStructure<float>* multiply(const DataStructure<float>* a, const float b) const override {
+    const auto& a_raw = a->get_raw_data();
+    auto res_raw = vec<float>(a->get_data_size(), 0);
+    for (int i = 0; i < a->get_data_size(); i++) {
+      res_raw[i] = a_raw[i] * b;
     }
-    return a;
+    Vector_mml<float>* res = new Vector_mml<float>(a->get_shape(), res_raw);
+    return res;
   }
 
-  DataStructure<float>* divide(DataStructure<float>* a, const float b) const override {
-    for (int i = 0; i < a->get_size(); i++) {
-      vec<int> ind = vec<int>{i};
-      a->set(ind, a->get(ind) / b);
+  DataStructure<float>* divide(const DataStructure<float>* a, const float b) const override {
+    const auto& a_raw = a->get_raw_data();
+    auto res_raw = vec<float>(a->get_data_size(), 0);
+    for (int i = 0; i < a->get_data_size(); i++) {
+      res_raw[i] = a_raw[i] / b;
     }
-    return a;
+    Vector_mml<float>* res = new Vector_mml<float>(a->get_shape(), res_raw);
+    return res;
   }
 
-  bool equal(DataStructure<float>* a, const DataStructure<float>* b) const override {
-    return a->equals(*b);
+  bool equals(const DataStructure<float>* a, const DataStructure<float>* b) const override {
+    if (bool same_shape = a->get_shape() == b->get_shape(); !same_shape) {
+      return false;
+    }
+    const auto& a_data = a->get_raw_data();
+    const auto& b_data = b->get_raw_data();
+    for (int i = 0; i < a->get_data_size(); i++) {
+      if (!f_eq(a_data[i], b_data[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+ private:
+  bool f_eq(float a, float b) const {
+    return std::abs(a - b) < 1e-2f;
+  }
+
+  bool d_eq(double a, double b) const {
+    return std::abs(a - b) < 1e-2;
   }
 };
