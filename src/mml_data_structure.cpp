dd94dc1ca8c8c30599338bc885cd7f410353c5b8
Tim <timcarlsson0203@gmail.com>
2025-02-23 15:40:59 +0100

    Fix bugs

--- src/mml_data_structure.cpp
+++ src/mml_data_structure.cpp
@@ -16,12 +16,12 @@ MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape)
 template <typename T>
 MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape, const vec<T> data)
 {
-    if (shape.size() <= 0) throw std::invalid_argument("Shape isn't valid")
+    if (shape.size() <= 0) throw std::invalid_argument("Shape isn't valid");
 
     int data_size = 1;
     for (int i : shape) data_size *= i;
 
-    if (data_size != data.size()) throw std::invalid_argument("The size of the data and the desired shape does not match")
+    if (data_size != data.size()) throw std::invalid_argument("The size of the data and the desired shape does not match");
 
     this->shape = shape;
     this->data = data;
@@ -30,8 +30,8 @@ MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape, const vec<T> data
 template <typename T>
 T MML_DataStructure<T>::get(const vec<int>& indices) const
 {
-    if (indices.size() == 0) throw std::invalid_argument("Invalid indices")
-    if (indices.size() != this->shape.size()) throw std::invalid_argument("The diemensions of the indices does not match with the data structure")
+    if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
+    if (indices.size() != this->shape.size()) throw std::invalid_argument("The diemensions of the indices does not match with the data structure");
     
 }
 
@@ -44,11 +44,10 @@ void MML_DataStructure<T>::set(const vec<int>& indices, T value)
 template <typename T>
 void MML_DataStructure<T>::set_data(const vec<T> data)
 {
-    if (this->get_size() != data.size()) throw std::invalid_argument("The size of the data provided mismatch with the size of the data structure")
+    if (this->get_size() != data.size()) throw std::invalid_argument("The size of the data provided mismatch with the size of the data structure");
     
     for (int i : this->data.size())
     {
-        this.set
     }
 }
 
