f0db2d01a7ad78d0051956da24a88445c9689ad3
Elias Nilsson <elinil0221@gmail.com>
2025-02-23 22:44:47 +0100

    add return of model class by smart pointer

---
+++ src/include/a_data_parser.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "data.hpp"
+#include "a_model.hpp"
+
+/**
+ * @class DataParser
+ * @brief Abstract class for parsing JSON data of a model into a Model Object.
+ */
+template <typename T>
+class DataParser
+{
+  public:
+    /**
+     * @brief Parses JSON data of a model into a Model object.
+     * 
+     * @param data JSON data of a Model.
+     */
+    virtual std::unique_ptr<Model<T>> parse(const Data& data) const = 0;
+    
+    /// @brief Virtual destructor for cleanup.
+    virtual ~DataParser() = default;
+};
