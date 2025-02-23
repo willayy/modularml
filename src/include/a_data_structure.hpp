b4a7cb956f68445a1029872dc0fb2c202eb8e23d
William Norland <williamnorland@gmail.com>
2025-02-23 22:13:35 +0100

    Refactor and fix data struture

--- src/include/a_data_structure.hpp
+++ src/include/a_data_structure.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include "globals.hpp"
 
 #define ASSERT_ALLOWED_TYPES(T) static_assert(std::is_arithmetic_v<T>, "Data structure type must be an arithmetic type.")
@@ -16,7 +18,7 @@ class DataStructure {
   virtual ~DataStructure() = default;
 
   /// @brief Set the data of the data structure.
-  /// @param data a vector of data to set in the data structure.
+  /// @param data a vector of data to get_mutable_elem in the data structure.
   virtual void set_data(const vec<T> data) = 0;
 
   /// @brief Set all elements in the data structure to zero.
@@ -24,7 +26,7 @@ class DataStructure {
 
   /// @brief Get the shape of the data structure.
   /// @return a vector of integers representing the shape.
-  virtual const vec<int> &get_shape() const = 0;
+  virtual const vec<int>& get_shape() const = 0;
 
   /// @brief Get the shape as a string. E.g. [2, 3, 4].
   /// @return a string representation of the shape.
@@ -32,20 +34,18 @@ class DataStructure {
 
   /// @brief Gets the amount of elements in the data structure.
   /// @return the size of the data structure as an integer.
-  virtual int get_size() const = 0;
+  virtual int get_data_size() const = 0;
 
-  /// @brief Compares the data structure with another data structure for equality.
-  /// @param other the other data structure to compare with.
-  /// @return true if the data structures are equal, false otherwise.
-  virtual bool equals(const DataStructure<T> &other) const = 0;
+  /// @brief Get the raw data of the data structure flattened in row-major order.
+  /// @return a vector of the raw data.
+  const virtual vec<T>& get_raw_data() const = 0;
 
   /// @brief Get an element from the data structure.
   /// @param indices a vector of integers representing the indices of the data structure.
   /// @return a value from the data structure.
-  virtual T get(const vec<int> &indices) const = 0;
+  virtual const T& get_elem(vec<int> indices) const = 0;
 
   /// @brief Set an element in the data structure.
   /// @param indices a vector of integers representing the indices of the data structure.
-  /// @param value the value to set in the data structure.
-  virtual void set(const vec<int> &indices, T value) = 0;
+  virtual T& get_mutable_elem(vec<int> indices) = 0;
 };
