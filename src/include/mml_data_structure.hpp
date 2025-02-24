d2f47c399347d10aa203db747c82bcd33d1ac71f
Tim <timcarlsson0203@gmail.com>
2025-02-24 14:15:14 +0100

    Format once again

--- src/include/mml_data_structure.hpp
+++ src/include/mml_data_structure.hpp
@@ -4,47 +4,47 @@
 
 template <typename T>
 class MML_DataStructure : public DataStructure<T> {
- public:
-  /**
-   * @brief Constructor for the MML_DataStructure class
-   * @param shape A vector defining the diemensions of the data
-   */
-  MML_DataStructure(const vec<int> &shape);
+   public:
+    /**
+     * @brief Constructor for the MML_DataStructure class
+     * @param shape A vector defining the diemensions of the data
+     */
+    MML_DataStructure(const vec<int> &shape);
 
-  /**
-   * @brief Constructor for the MML_DataStructure class
-   * @param shape A vector defining the diemensions of the data
-   * @param data A vector containing the data for the structure
-   */
-  MML_DataStructure(const vec<int> &shape, const vec<T> data);
+    /**
+     * @brief Constructor for the MML_DataStructure class
+     * @param shape A vector defining the diemensions of the data
+     * @param data A vector containing the data for the structure
+     */
+    MML_DataStructure(const vec<int> &shape, const vec<T> data);
 
-  void set_data(const vec<T> data) override;
+    void set_data(const vec<T> data) override;
 
-  void set_zero() override;
+    void set_zero() override;
 
-  const vec<int> get_shape() const override;
+    const vec<int> get_shape() const override;
 
-  const string get_shape_str() const override;
+    const string get_shape_str() const override;
 
-  int get_size() const override;
+    int get_size() const override;
 
-  bool equals(const MML_DataStructure<T> &other) const override;
+    bool equals(const MML_DataStructure<T> &other) const override;
 
-  T get(const vec<int> &indices) const override;
+    T get(const vec<int> &indices) const override;
 
-  void set(const vec<int> &indices, T value) override;
+    void set(const vec<int> &indices, T value) override;
 
-  void set(const int index, T value) override;
+    void set(const int index, T value) override;
 
- private:
-  std::vector<int> shape;
-  std::vector<T> data;
+   private:
+    std::vector<int> shape;
+    std::vector<T> data;
 
-  /**
-   * @brief Given the indices and the shape of the data structure calculates the
-   * offset that the indices represent.
-   * @param indices A vector of integers that each represent a direction in a
-   * diemension.
-   */
-  int calc_offset(const vec<int> &indices) const;
+    /**
+     * @brief Given the indices and the shape of the data structure calculates the
+     * offset that the indices represent.
+     * @param indices A vector of integers that each represent a direction in a
+     * diemension.
+     */
+    int calc_offset(const vec<int> &indices) const;
 };
