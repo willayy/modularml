0ac40f6e04728f6ee1fad667c99c3d39330e7c3c
Tim <timcarlsson0203@gmail.com>
2025-02-24 13:13:58 +0100

    Fix formatting, had forgotten a pragma in one file

--- src/include/mml_data_structure.hpp
+++ src/include/mml_data_structure.hpp
@@ -3,50 +3,48 @@
 #include "a_data_structure.hpp"
 
 template <typename T>
-class MML_DataStructure : public DataStructure<T>
-{ 
-  public:
+class MML_DataStructure : public DataStructure<T> {
+ public:
+  /**
+   * @brief Constructor for the MML_DataStructure class
+   * @param shape A vector defining the diemensions of the data
+   */
+  MML_DataStructure(const vec<int> &shape);
 
-    /**
-     * @brief Constructor for the MML_DataStructure class
-     * @param shape A vector defining the diemensions of the data
-     */
-    MML_DataStructure(const vec<int>& shape);
+  /**
+   * @brief Constructor for the MML_DataStructure class
+   * @param shape A vector defining the diemensions of the data
+   * @param data A vector containing the data for the structure
+   */
+  MML_DataStructure(const vec<int> &shape, const vec<T> data);
 
-    /**
-     * @brief Constructor for the MML_DataStructure class
-     * @param shape A vector defining the diemensions of the data
-     * @param data A vector containing the data for the structure
-     */
-    MML_DataStructure(const vec<int>& shape, const vec<T> data);
+  void set_data(const vec<T> data) override;
 
-    void set_data(const vec<T> data) override;
-    
-    void set_zero() override;
+  void set_zero() override;
 
-    const vec<int> get_shape() const override;
+  const vec<int> get_shape() const override;
 
-    const string get_shape_str() const override;
-    
-    int get_size() const override;
-    
-    bool equals(const MML_DataStructure<T> &other) const override;
-    
-    T get(const vec<int> &indices) const override;
+  const string get_shape_str() const override;
 
-    void set(const vec<int> &indices, T value) override;
+  int get_size() const override;
 
-    void set(const int index, T value) override;
+  bool equals(const MML_DataStructure<T> &other) const override;
 
+  T get(const vec<int> &indices) const override;
 
-  private:
-    std::vector<int> shape;
-    std::vector<T> data;
+  void set(const vec<int> &indices, T value) override;
 
-    /**
-     * @brief Given the indices and the shape of the data structure calculates the offset that the indices represent.
-     * @param indices A vector of integers that each represent a direction in a diemension.
-     */
-    int calc_offset(const vec<int> &indices) const;
-    
-}; 
+  void set(const int index, T value) override;
+
+ private:
+  std::vector<int> shape;
+  std::vector<T> data;
+
+  /**
+   * @brief Given the indices and the shape of the data structure calculates the
+   * offset that the indices represent.
+   * @param indices A vector of integers that each represent a direction in a
+   * diemension.
+   */
+  int calc_offset(const vec<int> &indices) const;
+};
--- src/mml_data_structure.cpp
+++ src/mml_data_structure.cpp
@@ -1,134 +1,141 @@
 #include "mml_data_structure.hpp"
 
 template <typename T>
-MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape)
-{
-    if (shape.size() <= 0) throw std::invalid_argument("Shape is invalid");
+MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape) {
+  if (shape.size() <= 0) throw std::invalid_argument("Shape is invalid");
 
-    this->shape = shape;
-    
-    // Calculate the size using the diemensions provided
-    int data_size = 1;
-    for (int i : shape) data_size *= i;
-    this->data = vec<T>(data_size);
+  this->shape = shape;
+
+  // Calculate the size using the diemensions provided
+  int data_size = 1;
+  for (int i : shape) data_size *= i;
+  this->data = vec<T>(data_size);
 }
 
 template <typename T>
-MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape, const vec<T> data)
-{
-    if (shape.size() <= 0) throw std::invalid_argument("Shape isn't valid");
+MML_DataStructure<T>::MML_DataStructure(const vec<int>& shape,
+                                        const vec<T> data) {
+  if (shape.size() <= 0) throw std::invalid_argument("Shape isn't valid");
 
-    int data_size = 1;
-    for (int i : shape) data_size *= i;
+  int data_size = 1;
+  for (int i : shape) data_size *= i;
 
-    if (data_size != data.size()) throw std::invalid_argument("The size of the data and the shape does not match");
+  if (data_size != data.size())
+    throw std::invalid_argument(
+        "The size of the data and the shape does not match");
 
-    this->shape = shape;
-    this->data = data;
+  this->shape = shape;
+  this->data = data;
 }
 
 template <typename T>
-T MML_DataStructure<T>::get(const vec<int>& indices) const
-{
-    if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
-    if (indices.size() != this->shape.size()) throw std::invalid_argument("The diemensions of the indices does not match with the data structure");
-    
-    return this->data.at(calc_offset(indices));
+T MML_DataStructure<T>::get(const vec<int>& indices) const {
+  if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
+  if (indices.size() != this->shape.size())
+    throw std::invalid_argument(
+        "The diemensions of the indices does not match with the data "
+        "structure");
+
+  return this->data.at(calc_offset(indices));
 }
 
 template <typename T>
-void MML_DataStructure<T>::set(const vec<int>& indices, T value)
-{
-    if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
-    if (indices.size() != this->shape.size()) throw std::invalid_argument("The diemensions of the indices does not match with the data structure");
+void MML_DataStructure<T>::set(const vec<int>& indices, T value) {
+  if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
+  if (indices.size() != this->shape.size())
+    throw std::invalid_argument(
+        "The diemensions of the indices does not match with the data "
+        "structure");
 
-    this->data[calc_offset(indices)] = value;
+  this->data[calc_offset(indices)] = value;
 }
 
 template <typename T>
-void MML_DataStructure<T>::set(const int index, T value)
-{
-    if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
-    if (indices.size() != this->shape.size()) throw std::invalid_argument("The diemensions of the indices does not match with the data structure");
+void MML_DataStructure<T>::set(const int index, T value) {
+  if (indices.size() == 0) throw std::invalid_argument("Invalid indices");
+  if (indices.size() != this->shape.size())
+    throw std::invalid_argument(
+        "The diemensions of the indices does not match with the data "
+        "structure");
 
-    this->data[index] = value;
+  this->data[index] = value;
 }
 
+template <typename T>
+void MML_DataStructure<T>::set_data(const vec<T> new_data) {
+  // TODO: Parallelize
+  if (this->get_size() != data.size())
+    throw std::invalid_argument(
+        "The size of the data provided mismatch with the size of the data "
+        "structure");
+
+  for (int i : this->data.size()) {
+    this->data[i] = new_data[i];
+  }
+}
 
 template <typename T>
-void MML_DataStructure<T>::set_data(const vec<T> new_data)
-{
-    if (this->get_size() != data.size()) throw std::invalid_argument("The size of the data provided mismatch with the size of the data structure");
-    
-    for (int i : this->data.size())
+void MML_DataStructure<T>::set_zero() {
+  // TODO: Parallelize
+  for (int i : this->data.size()) {
+    this->data[i] = 0;
+  }
+}
+
+template <typename T>
+const vec<int> MML_DataStructure<T>::get_shape() const {
+  return this->shape;
+}
+
+template <typename T>
+const string MML_DataStructure<T>::get_shape_str() const {
+  string result = "[";
+  for (int i = 0; i < this->shape.size(); ++i) {
+    if (i + 1 == this->shape.size())
+      result += std::to_string(i);
+    else
+      result += (std::to_string(i) + ", ");
+  }
+  result += "]";
+  return result;
+}
+
+template <typename T>
+int MML_DataStructure<T>::get_size() const {
+  return this->data
+      .size();  // This assumes that the structure is populated by data
+}
+
+template <typename T>
+bool MML_DataStructure<T>::equals(const MML_DataStructure<T>& other) const {
+  if (this->data.size() ==
+      other->data.size())  // if the number of diemensions are not the same then
+                           // they can't be equal
+  {
+    for (int i = 0; i < this->data.size(); ++i) {
+      if (this->data.at(i) != other->data.size())
+        return false;  // Check the diemensions of the structures
+    }
+    return true;
+  }
+  return false;
+}
+
+template <typename T>
+int MML_DataStructure<T>::calc_offset(const vec<int>& indices) const {
+  int offset = 0;
+  int factor = 1;
+  for (int i = indices.size() - 1; i >= 0; --i)  // Iterates backwards
+  {
+    if (indices.at(i) < 0 ||
+        indices.at(i) >=
+            this->shape.at(i))  // Check that the index isn't out of range
     {
-        this->data[i] = new_data[i];
+      throw std::out_of_range("Index in indices vector are out of range " +
+                              std::to_string(i));
     }
+    offset += indices.at(i) * factor;
+    factor *= this->shape(i);
+  }
+  return offset;
 }
-
-template <typename T>
-void MML_DataStructure<T>::set_zero()
-{
-    for (int i : this->data.size())
-    {
-        this->data[i] = 0;
-    }
-}
-
-template <typename T>
-const vec<int> MML_DataStructure<T>::get_shape() const 
-{
-    return this->shape;
-}
-
-template <typename T>
-const string MML_DataStructure<T>::get_shape_str() const
-{
-    string result = "[";
-    for (int i = 0; i < this->shape.size(); ++i)
-    {
-        if (i + 1 == this->shape.size()) result += std::to_string(i);
-        else result += (std::to_string(i) + ", ");
-    }
-    result += "]";
-    return result;
-}
-
-template <typename T>
-int MML_DataStructure<T>::get_size() const
-{
-    return this->data.size(); // This assumes that the structure is populated by data
-}
-
-template <typename T>
-bool MML_DataStructure<T>::equals(const MML_DataStructure<T> &other) const
-{
-    if (this->data.size() == other->data.size()) // if the number of diemensions are not the same then they can't be equal
-    {
-        for (int i = 0; i < this->data.size(); ++i)
-        {
-            if (this->data.at(i) != other->data.size()) return false; // Check the diemensions of the structures 
-        }
-        return true;
-    }
-    return false;
-}
-
-template <typename T>
-int MML_DataStructure<T>::calc_offset(const vec<int> &indices) const
-{
-    int offset = 0;
-    int factor = 1;
-    for (int i = indices.size() - 1; i >= 0; --i) // Iterates backwards
-    {   
-        if (indices.at(i) < 0 || indices.at(i) >= this->shape.at(i)) // Check that the index isn't out of range
-        {
-            throw std::out_of_range("Index in indices vector are out of range " + std::to_string(i));
-        }
-        offset += indices.at(i) * factor;
-        factor *= this->shape(i);
-    }
-    return offset;
-}
-
-
