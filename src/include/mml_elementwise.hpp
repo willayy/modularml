0dd688ae47ccd5aaf0ff21d344b25dfabab2b730
Måns Bremer <mans.bremer@gmail.com>
2025-02-25 13:04:04 +0100

    Comment

--- src/include/mml_elementwise.hpp
+++ src/include/mml_elementwise.hpp
@@ -14,6 +14,8 @@
  * @return A reference to the modified tensor.
  */
 Tensor<float>& elementwise_apply(Tensor<float>& t, float (*f)(float)){
+    // This function can be made way more efficent by the use of multi-threading
+    // I intend on making that an improvement in the future
     for (int i = 0; i < t.get_shape()[0]; i++) {
         for (int j = 0; j < t.get_shape()[1]; j++) {
         t[{i, j}] = f(t[{i, j}]);
