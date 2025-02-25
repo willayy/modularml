91fd32ab686492b34970f3f8986652a3af796d02
Måns Bremer <mans.bremer@gmail.com>
2025-02-25 12:46:04 +0100

    Add elementwise_apply function

---
+++ src/include/mml_elementwise.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "tensor.hpp"
+
+/**
+ * @brief Applies a given function element-wise to a tensor.
+ * 
+ * This function iterates over each element in the given tensor and applies
+ * the provided function to each element, modifying the tensor in place.
+ * 
+ * @param t The tensor to which the function will be applied.
+ * @param f A pointer to the function that will be applied to each element of the tensor.
+ *          The function should take a float as input and return a float.
+ * @return A reference to the modified tensor.
+ */
+Tensor<float>& elementwise_apply(Tensor<float>& t, float (*f)(float)){
+    for (int i = 0; i < t.get_shape()[0]; i++) {
+        for (int j = 0; j < t.get_shape()[1]; j++) {
+        t[{i, j}] = f(t[{i, j}]);
+        }
+    }
+    return t;
+}
