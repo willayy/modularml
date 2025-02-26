843705e5c4db3987e6cedee210a986cc2aa0265b
William Norland <williamnorland@gmail.com>
2025-02-26 17:57:02 +0100

    Add std:: to accumulate

--- src/mml_tensors.cpp
+++ src/mml_tensors.cpp
@@ -6,7 +6,7 @@
 /// @param shape The shape of the tensor.
 /// @return A new tensor with the given shape and all elements set to zero.
 Tensor<float> tensor_mll(const vector<int> shape) {  // NOSONAR - function signature is correct
-  auto size = accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>());
+  auto size = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>());
   auto ds = make_unique<Vector_mml<float>>(size);
   auto am = make_unique<Arithmetic_mml>();
   return Tensor<float>(move(ds), move(am), shape);
