18b19d0bb97ebdc7fb260f57889eace992808bd5
William Norland <williamnorland@gmail.com>
2025-02-27 13:54:46 +0100

    Implement new abstract arithmetics

--- src/include/a_arithmetic_module.hpp
+++ src/include/a_arithmetic_module.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
-// Include memory for unique_ptr
-#include <type_traits>
+#include "globals.hpp"
+#include "tensor.hpp"
 
-#include "a_data_structure.hpp"
+#define ASSERT_ALLOWED_TYPES_AR(T) static_assert(std::is_arithmetic_v<T>, "ArithmeticModule type must be arithmetic.")
 
-#define ASSERT_ALLOWED_TYPES(T) static_assert(std::is_arithmetic_v<T>, "Data structure type must be an arithmetic type.")
-
-/// @brief A module for performing simple arithmetic operations on data structures.
+/// @brief A module for performing simple arithmetic operations on tensor structures.
 /// @tparam T the data type (numeric).
-/// @tparam D the data structure type (DataStructure).
 template <typename T>
 class ArithmeticModule {
  public:
@@ -25,35 +22,30 @@ class ArithmeticModule {
   /// @brief Abstract destructor for ArithmeticModule class.
   virtual ~ArithmeticModule() = default;
 
-  /// @brief Add two data structures.
-  /// @param a The first data structure.
-  /// @param b The second data structure.
-  /// @return The result of adding the two data structures.
-  virtual unique_ptr<DataStructure<T>> add(const unique_ptr<DataStructure<T>> a, const unique_ptr<DataStructure<T>> b) const = 0;
+  /// @brief Add two tensor structures.
+  /// @param a The first tensor.
+  /// @param b The second tensor structure.
+  /// @param c The tensor structure to store the result.
+  virtual void add(const shared_ptr<Tensor<T>> a, const shared_ptr<Tensor<T>> b, shared_ptr<Tensor<T>> c) const = 0;
 
-  /// @brief Subtract one data structure from another.
-  /// @param a The data structure to subtract from.
-  /// @param b The data structure to subtract.
-  /// @return The result of subtracting the second data structure from the first.
-  virtual unique_ptr<DataStructure<T>> subtract(const unique_ptr<DataStructure<T>> a, const unique_ptr<DataStructure<T>> b) const = 0;
+  /// @brief Subtract one tensor structure from another.
+  /// @param a The tensor structure to subtract from.
+  /// @param b The tensor structure to subtract.
+  /// @param c The tensor structure to store the result.
+  virtual void subtract(const shared_ptr<Tensor<T>> a, const shared_ptr<Tensor<T>> b, shared_ptr<Tensor<T>> c) const = 0;
 
-  /// @brief Multiply a data structure by a scalar.
-  /// @param a The data structure.
+  /// @brief Multiply a tensor structure by a scalar.
+  /// @param a The tensor structure.
   /// @param b The scalar value.
-  /// @return The result of multiplying the data structure by the scalar.
-  virtual unique_ptr<DataStructure<T>> multiply(const unique_ptr<DataStructure<T>> a, const T b) const = 0;
+  /// @param c The tensor structure to store the result.
+  virtual void multiply(const shared_ptr<Tensor<T>> a, const T b, shared_ptr<Tensor<T>> c) const = 0;
 
-  /// @brief Divide a data structure by a scalar.
-  /// @param a The data structure.
-  /// @param b The scalar value.
-  /// @return The result of dividing the data structure by the scalar.
-  virtual unique_ptr<DataStructure<T>> divide(const unique_ptr<DataStructure<T>> a, const T b) const = 0;
+  /// @brief Check if two tensor structures are equal.
+  /// @param a The first tensor structure.
+  /// @param b The second tensor structure.
+  virtual bool equals(const shared_ptr<Tensor<T>> a, const shared_ptr<Tensor<T>> b) const = 0;
 
-  /// @brief Check if two data structures are equal.
-  /// @param a The first data structure.
-  /// @param b The second data structure.
-  /// @return True if the data structures are equal, false otherwise.
-  virtual bool equals(const unique_ptr<DataStructure<T>> a, const unique_ptr<DataStructure<T>> b) const = 0;
-
-  virtual unique_ptr<ArithmeticModule<T>> clone() const = 0;
+  /// @brief Clone the arithmetic module.
+  /// @return a unique pointer to a new arithmetic module.
+  virtual shared_ptr<ArithmeticModule<T>> clone() const = 0;
 };
