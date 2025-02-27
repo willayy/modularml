1e255941b15b6aab8cacc8254eede317fa28c124
Måns Bremer <mans.bremer@gmail.com>
2025-02-27 12:34:13 +0100

    Add mml_TensorFunction_Swish.hpp

---
+++ src/include/mml_TensorFunction_Swish.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cmath>
+#include <modularml>
+
+class mml_TensorFunction_Swish : public TensorFunction<float> {
+ private:
+  mutable mml_elementwise<float> elementwise;  // Determines what version of elementwise to use
+
+ public:
+  /**
+  * @brief Apply the Swish function to the given tensor.
+
+  * @param t The tensor to which the function will be applied.
+  * @return A new tensor with the Swish function applied to each element.
+ */
+  Tensor<float> func(const Tensor<float>& t) const {
+    auto tensor = t;
+    return elementwise.apply(tensor, [](float x) {
+      return x / (1.0f + std::exp(-x));  // Compute the Swish function
+    });
+  }
+
+  /**
+   * @brief Apply the derivative of the Swish function to the tensor.
+
+   * @param t The tensor to which the function will be applied.
+   * @return A new tensor with the derivative of Swish applied to each element.
+  */
+  Tensor<float> derivative(const Tensor<float>& t) const {
+    auto tensor = t;
+    return elementwise.apply(tensor, [](float x) {
+      float sigmoid_x = 1.0f / (1.0f + std::exp(-x));  // Compute the derivative of the Swish function
+      return sigmoid_x + x * sigmoid_x * (1.0f - sigmoid_x);
+    });
+  }
+
+  /**
+   * @brief Apply an approximation of the primitive of the Swish function to the given tensor.
+
+   * @param t The tensor to which the function will be applied.
+   * @return A new tensor with the approximate primitive of Swish applied to each element.
+  */
+  Tensor<float> primitive(const Tensor<float>& t) const {
+    // This has to be an approximation as there is no known closed-form solution
+    // Therefor this uses the Logistic Sigmoid Integral function.
+    auto tensor = t;
+    return elementwise.apply(tensor, [](float x) {
+      float sigmoid_x = 1.0f / (1.0f + std::exp(-x));        // Compute σ(x)
+      return x * sigmoid_x + std::log(1.0f + std::exp(-x));  // Compute integral of Swish
+    });
+  }
+};
