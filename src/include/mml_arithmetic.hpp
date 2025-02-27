f2c52d57c739d973dfbbbaf2d4aa92a16b2fc1fc
William Norland <williamnorland@gmail.com>
2025-02-27 13:56:43 +0100

    Fix new arithmetic implementation

--- src/include/mml_arithmetic.hpp
+++ src/include/mml_arithmetic.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "a_arithmetic_module.hpp"
-#include "a_data_structure.hpp"
 #include "globals.hpp"
-#include "mml_vector.hpp"
+#include "tensor.hpp"
 
-class Arithmetic_mml : public ArithmeticModule<float> {
+template <typename T>
+class Arithmetic_mml : public ArithmeticModule<T> {
  public:
-  
   // Override default constructor
   Arithmetic_mml() = default;
 
@@ -20,65 +19,42 @@ class Arithmetic_mml : public ArithmeticModule<float> {
   // Override destructor
   ~Arithmetic_mml() override = default;
 
-  unique_ptr<DataStructure<float>> add(const unique_ptr<DataStructure<float>> a, const unique_ptr<DataStructure<float>> b) const override {
-    const int size = a->get_size();
-    auto res_raw = vector<float>(size, 0);
+  void add(const shared_ptr<Tensor<T>> a, const shared_ptr<Tensor<T>> b, shared_ptr<Tensor<T>> c) const override {
+    const auto size = a->get_size();
     for (int i = 0; i < size; i++) {
-      res_raw[i] = a->get_elem(i) + b->get_elem(i);
+      (*c)[i] = (*a)[i] + (*b)[i];
     }
-    return make_unique<Vector_mml<float>>(res_raw);
   }
 
-  unique_ptr<DataStructure<float>> subtract(const unique_ptr<DataStructure<float>> a, const unique_ptr<DataStructure<float>> b) const override {
-    const int size = a->get_size();
-    auto res_raw = vector<float>(size, 0);
+  void subtract(const shared_ptr<Tensor<T>> a, const shared_ptr<Tensor<T>> b, shared_ptr<Tensor<T>> c) const override {
+    const auto size = a->get_size();
     for (int i = 0; i < size; i++) {
-      res_raw[i] = a->get_elem(i) - b->get_elem(i);
+      (*c)[i] = (*a)[i] - (*b)[i];
     }
-    return make_unique<Vector_mml<float>>(res_raw);
   }
 
-  unique_ptr<DataStructure<float>> multiply(const unique_ptr<DataStructure<float>> a, const float b) const override {
-    const int size = a->get_size();
-    auto res_raw = vector<float>(size, 0);
+  void multiply(const shared_ptr<Tensor<T>> a, const T b, shared_ptr<Tensor<T>> c) const override {
+    const auto size = a->get_size();
     for (int i = 0; i < size; i++) {
-      res_raw[i] = a->get_elem(i) * b;
+      (*c)[i] = (*a)[i] * b;
     }
-    return make_unique<Vector_mml<float>>(res_raw);
   }
 
-  unique_ptr<DataStructure<float>> divide(const unique_ptr<DataStructure<float>> a, const float b) const override {
-    const int size = a->get_size();
-    auto res_raw = vector<float>(size, 0);
-    for (int i = 0; i < size; i++) {
-      res_raw[i] = a->get_elem(i) / b;
-    }
-    return make_unique<Vector_mml<float>>(res_raw);
-  }
-
-  bool equals(const unique_ptr<DataStructure<float>> a, const unique_ptr<DataStructure<float>> b) const override {
-    if (bool same_shape = a->get_size() == b->get_size(); !same_shape) {
+  bool equals(const shared_ptr<Tensor<T>> a, const shared_ptr<Tensor<T>> b) const override {
+    if (a->get_size() != b->get_size() || a->get_shape() != b->get_shape()) {
       return false;
-    }
-    const int size = a->get_size();
-    for (int i = 0; i < size; i++) {
-      if (!f_eq(a->get_elem(i), b->get_elem(i))) {
-        return false;
+    } else {
+      const auto size = a->get_size();
+      for (int i = 0; i < size; i++) {
+        if ((*a)[i] == (*b)[i]) {
+          return false;
+        }
       }
+      return true;
     }
-    return true;
   }
 
-  unique_ptr<ArithmeticModule<float>> clone() const override {
-    return make_unique<Arithmetic_mml>(*this);
-  }
-
- private:
-  bool f_eq(float a, float b) const {
-    return std::abs(a - b) < 1e-2f;
-  }
-
-  bool d_eq(double a, double b) const {
-    return std::abs(a - b) < 1e-2;
+  shared_ptr<ArithmeticModule<T>> clone() const override {
+    return make_shared<Arithmetic_mml>(*this);
   }
 };
