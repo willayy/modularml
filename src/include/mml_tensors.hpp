01b668437932351fb85117084bdf4037843e0480
William Norland <williamnorland@gmail.com>
2025-02-27 13:57:13 +0100

    made mml_tensors into template and made ptr shared

--- src/include/mml_tensors.hpp
+++ src/include/mml_tensors.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
 #include "globals.hpp"
-#include "mml_arithmetic.hpp"
-#include "mml_vector.hpp"
 #include "tensor.hpp"
+#include "mml_vector.hpp"
 
-Tensor<float> tensor_mll(vector<int> shape);
+/// @brief Initializes a new tensor with the given shape and all elements set to zero.
+/// @param shape The shape of the tensor.
+/// @return A new tensor with the given shape and all elements set to zero.
+template <typename T>
+shared_ptr<Tensor<T>> tensor_mml(const initializer_list<int> shape) {  // NOSONAR - function signature is correct
+  auto size = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>());
+  auto ds = make_shared<Vector_mml<T>>(size);
+  return make_shared<Tensor<T>>(move(ds), shape);
+}
 
-Tensor<float> tensor_mll(const vector<int> shape, const vector<float> data);
+/// @brief Initializes a new tensor with the given shape and data.
+/// @param shape The shape of the tensor.
+/// @param data A reference to the data to be set in the tensor.
+/// @return A new tensor with the given shape and data.
+template <typename T>
+shared_ptr<Tensor<T>> tensor_mml(const initializer_list<int> &shape, const initializer_list<T> &data) {  // NOSONAR - function signature is correct
+  auto ds = make_shared<Vector_mml<T>>(data);
+  return make_shared<Tensor<T>>(move(ds), shape);
+}
