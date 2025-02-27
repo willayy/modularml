e016ea85da431705083172ff031c78b9c83aeb20
Måns Bremer <mans.bremer@gmail.com>
2025-02-27 11:17:06 +0100

    Change test to reflect new elementwise implementation

--- tests/test_mml_elementwise.cpp
+++ tests/test_mml_elementwise.cpp
@@ -1,28 +1,28 @@
 #include <cassert>
 #include <iostream>
-#include <vector>
 #include <modularml>
+#include <vector>
 
 #define assert_msg(name, condition)                         \
-    if (!(condition)) {                                       \
-        std::cerr << "Assertion failed: " << name << std::endl; \
-    }                                                         \
-    assert(condition);                                        \
-    std::cout << name << ": " << (condition ? "Passed" : "Failed") << std::endl;
+  if (!(condition)) {                                       \
+    std::cerr << "Assertion failed: " << name << std::endl; \
+  }                                                         \
+  assert(condition);                                        \
+  std::cout << name << ": " << (condition ? "Passed" : "Failed") << std::endl;
 
-float square(float x) {
-    return x * x;
-}
+float square(float x) { return x * x; }
 
 int main() {
-    Tensor<float> t1 = tensor_mll({3, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9});
-    Tensor<float> t2 = tensor_mll({3, 3}, {1, 4, 9, 16, 25, 36, 49, 64, 81});
+  mml_elementwise<float> elementwise;  // Determines what version of elementwise to use
 
-    // Test elementwise_apply function
-    elementwise_apply(t1, square);
-    assert_msg("Elementwise apply test", t1 == t2);
+  Tensor<float> t1 = tensor_mll({3, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9});
+  Tensor<float> t2 = tensor_mll({3, 3}, {1, 4, 9, 16, 25, 36, 49, 64, 81});
 
-    std::cout << "All tests completed!" << std::endl;
+  // Test elementwise_apply function
+  elementwise.apply(t1, square);
+  assert_msg("Elementwise apply test", t1 == t2);
 
-    return 0;
+  std::cout << "All tests completed!" << std::endl;
+
+  return 0;
 }
