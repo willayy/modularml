fceb656a375b067e70b5cdb5750c9d20d656a203
Måns Bremer <mans.bremer@gmail.com>
2025-02-28 19:33:32 +0100

    Make tensors are close compatible with any arithmetic type

--- src/include/mml_tensors_are_close.hpp
+++ src/include/mml_tensors_are_close.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <type_traits>
 #include "tensor.hpp"
 
 /**
@@ -11,17 +12,29 @@
  * between corresponding elements is within the specified tolerance. If any pair of elements differ by
  * more than the tolerance, the function returns false. Otherwise, it returns true.
  *
+ * @tparam T The data type of the tensor elements (must be an arithmetic type).
  * @param t1 The first tensor to compare.
  * @param t2 The second tensor to compare.
- * @param tolerance The maximum allowed difference between corresponding elements of the tensors seen as a percentage. Default is 0.01f.
+ * @param tolerance The maximum allowed difference between corresponding elements of the tensors seen as a percentage. Default is 0.01.
  * @return true if all corresponding elements of the tensors are within the specified tolerance, false otherwise.
  */
-static bool tensors_are_close(Tensor<float>& t1, Tensor<float>& t2, float tolerance = 0.01f) {
+template <typename T>
+static bool tensors_are_close(Tensor<T>& t1, Tensor<T>& t2, T tolerance = T(0.01)) {
+  static_assert(std::is_arithmetic<T>::value, "Tensor type must be an arithmetic type (int, float, double, etc.).");
+
+  if (t1.get_shape() != t2.get_shape()) {
+    std::cerr << "Error: Tensors have different shapes and cannot be compared!" << std::endl;
+    return false;
+  }
+
   for (int i = 0; i < t1.get_shape()[0]; i++) {
     for (int j = 0; j < t1.get_shape()[1]; j++) {
-      if (std::abs(t1[{i, j}] - t2[{i, j}]) > std::abs(tolerance * t2[{i, j}])) {
-        std::cerr << "Difference of " << std::abs(t1[{i, j}] - t2[{i, j}]) << " found at (" << i << ", " << j << ") which is too large." << std::endl;
-        std::cerr << "Tolerance limit is " << std::abs(tolerance * t2[{i, j}]) << std::endl;
+      T diff = std::abs(t1[{i, j}] - t2[{i, j}]);
+      T tolerance_limit = std::abs(tolerance * t2[{i, j}]);
+
+      if (diff > tolerance_limit) {
+        std::cerr << "Difference of " << diff << " found at (" << i << ", " << j << ") which is too large." << std::endl;
+        std::cerr << "Tolerance limit is " << tolerance_limit << std::endl;
         return false;
       }
     }
