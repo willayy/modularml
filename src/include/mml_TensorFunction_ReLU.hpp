2e625890719bde227fcfab66999390ed5adc7ded
Måns Bremer <mans.bremer@gmail.com>
2025-02-28 20:12:25 +0100

    Make activation function classes support any arithmetic type

--- src/include/mml_TensorFunction_ReLU.hpp
+++ src/include/mml_TensorFunction_ReLU.hpp
@@ -1,46 +1,52 @@
 #pragma once
 
+#include <type_traits>
+
 #include "mml_elementwise.hpp"
 #include "tensor.hpp"
 
 /**
  * @class ReLU_mml
  * @brief A class that implements a tensor function for the ReLU function.
+ * @param T The data type of the tensor elements (must be an arithmetic type).
  */
-class ReLU_mml : public TensorFunction<float> {
+template <typename T>
+class ReLU_mml : public TensorFunction<T> {
  private:
-  mutable mml_elementwise<float> elementwise;  // Determines what version of elementwise to use
+  mutable mml_elementwise<T> elementwise;  // Determines what version of elementwise to use
 
  public:
+  static_assert(std::is_arithmetic<T>::value, "ReLU_mml requires an arithmetic type (float, double, int, etc.).");
+
   /**
    * @brief Apply the ReLU function to the given tensor.
-
+   *
    * @param t The tensor to which the function will be applied.
    * @return A new tensor with the ReLU function applied to each element.
-  */
-  Tensor<float> func(const Tensor<float>& t) const {
-    return elementwise.apply(t, [](float x) { return (x > 0.0f) ? x : 0.0f; });
+   */
+  Tensor<T> func(const Tensor<T>& t) const {
+    return elementwise.apply(t, [](T x) { return (x > T(0)) ? x : T(0); });
   }
 
   /**
    * @brief Apply the derivative of the ReLU function to the tensor.
-
+   *
    * @param t The tensor to which the function will be applied.
    * @return A new tensor with the derivative of ReLU applied to each element.
-  */
-  Tensor<float> derivative(const Tensor<float>& t) const {
-    return elementwise.apply(t, [](float x) {
-      return (x > 0.0f) ? 1.0f : 0.0f;  // Defaults to 0, like TensorFlow does
+   */
+  Tensor<T> derivative(const Tensor<T>& t) const {
+    return elementwise.apply(t, [](T x) {
+      return (x > T(0)) ? T(1) : T(0);  // Defaults to 0, like TensorFlow does
     });
   }
 
   /**
    * @brief Apply the primitive of the ReLU function to the given tensor.
-
+   *
    * @param t The tensor to which the function will be applied.
    * @return A new tensor with the primitive of ReLU applied to each element.
-  */
-  Tensor<float> primitive(const Tensor<float>& t) const {
-    return elementwise.apply(t, [](float x) { return (x > 0.0f) ? (x * x) / 2.0f : 0.0f; });
+   */
+  Tensor<T> primitive(const Tensor<T>& t) const {
+    return elementwise.apply(t, [](T x) { return (x > T(0)) ? (x * x) / T(2) : T(0); });
   }
 };
