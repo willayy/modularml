b4a05e17b1b03e3787c8dcf467588cd39c8ae52d
sanderc1 <sanderjcarlsson@gmail.com>
2025-03-02 18:29:52 +0100

    Fix syntax

--- src/include/mml_max_pooling_layer.hpp
+++ src/include/mml_max_pooling_layer.hpp
@@ -8,11 +8,11 @@ class MaxPoolingLayer : public Layer {
  public:
   MaxPoolingLayer(vector<int> p, vector<int> s, string p = "valid") : filter(p), stride(s), padding(p) {};
 
-  Tensor<T> tensor() const override {};
+  Tensor<T> tensor() const override;
 
-  std::unique_ptr<TensorFunction<T>> activation() const override {};
+  std::unique_ptr<TensorFunction<T>> activation() const override;
 
-  Tensor<T> forward(const Tensor<T>& input) const override {};
+  Tensor<T> forward(const Tensor<T>& input) const override;
 
  private:
   vector<int> filter;
