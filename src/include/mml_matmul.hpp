92a9cf5ccc3a7ef79d9d647171796cd206d11f9e
Tim <timcarlsson0203@gmail.com>
2025-03-02 15:19:04 +0100

    Remove .cpp file for MatMul, put everything into header file instead, forward() implemented using gemm

--- src/include/mml_matmul.hpp
+++ src/include/mml_matmul.hpp
@@ -2,6 +2,8 @@
 
 #include "a_layer.hpp"
 #include "globals.hpp"
+#include "mml_gemm.hpp"
+#include "mml_tensors.hpp"
 
 /**
  * @class MatMul
@@ -22,7 +24,7 @@ class MatMul : public Layer<T> {
      *
      * @param weight The weight tensor used in the matrix multiplication.
      */
-    MatMul(const Tensor<T>& weight);
+    MatMul(const Tensor<T>& weight) : weight(weight) {};
 
     /**
      * @brief Performs the forward pass of the MatMul layer.
@@ -32,7 +34,25 @@ class MatMul : public Layer<T> {
      * @param input The input tensor to be multiplied.
      * @return A unique pointer to the result tensor.
      */
-    Tensor<T> forward(const Tensor<T>& input) const override;
+    Tensor<T> forward(const Tensor<T>& input) const override {
+        auto A = make_shared<Tensor<T>>(input);
+        auto B = make_shared<Tensor<T>>(this->weight);
+        Tensor<T> result = tensor_mml<T>({input.get_shape().at(0), weight.get_shape().at(1)});
+        auto result_ptr = make_shared<Tensor<T>>(result);
+
+        shared_ptr<GemmModule<T>> gemm = make_shared<Gemm_mml<T>>();
+        gemm->gemm_inner_product(
+            0, 0,
+            input.get_shape().at(0), weight.get_shape().at(1), weight.get_shape().at(0),
+            1.0f,
+            make_shared<Tensor<T>>(input), input.get_shape().at(1), 
+            make_shared<Tensor<T>>(weight), weight.get_shape().at(1),
+            0.0f,
+            result_ptr, result_ptr->get_shape().at(1)
+        );
+
+        return *result_ptr;
+    };
 
     /**
      * @brief Returns the weight tensor used in the layer.
@@ -41,7 +61,9 @@ class MatMul : public Layer<T> {
      *
      * @return A tensor object representing the weights.
      */
-    Tensor<T> tensor() const override;
+    Tensor<T> tensor() const override {
+        return weight;
+    };
 
     /**
      * @brief Returns the activation function for the layer.
@@ -50,7 +72,18 @@ class MatMul : public Layer<T> {
      *
      * @return A nullptr since MatMul does not have an activation function.
      */
-    std::unique_ptr<TensorFunction<T>> activation() const override;
+    std::unique_ptr<TensorFunction<T>> activation() const override {
+        return nullptr;
+    };
+
+    /**
+     * @brief Returns the MatMul instance's weight tensor
+     * 
+     * @return A Tensor object
+     */
+    Tensor<T> get_weights() const {
+        return this->weight;
+    }
 
    private:
     Tensor<T> weight;  // The weight tensor used in the matrix multiplication
--- tests/test_mml_matmul.cpp
+++ tests/test_mml_matmul.cpp
@@ -1,9 +1,10 @@
-#include <cassert>
-#include <iostream>
+#include <gtest/gtest.h>
+
 #include <modularml>
 
-#include "mml_matmul.hpp"
-
-int main() {
-
+TEST(test_mul, test_constructor) {
+    Tensor<float> weights = tensor_mml<float>({2, 2}, {0.1, 0.2, 0.3, 0.4});
+    MatMul<float> matmul = MatMul<float>(weights);
+    
+    EXPECT_EQ(matmul.get_weights().get_shape(), vector<int>({2, 2}));
 }
