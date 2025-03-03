2f5ff3413c661d137cc9c9b37bb3050dc1f2c90d
sanderc1 <sanderjcarlsson@gmail.com>
2025-03-03 23:53:40 +0100

    Make variables protected

--- src/include/mml_pooling_layer.hpp
+++ src/include/mml_pooling_layer.hpp
@@ -17,7 +17,7 @@ class PoolingLayer : public Layer {
   virtual T pooling(const Tensor<T>& t, vector<int> shape, int element,
                     int channel, int in_row_start, int in_col_start) const = 0;
 
- private:
+ protected:
   vector<int> filter;
   vector<int> stride;
   string padding;
