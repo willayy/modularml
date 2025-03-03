033ff6e0604873c89dcddab2cc8d4b4e1d6a6603
sanderc1 <sanderjcarlsson@gmail.com>
2025-03-04 00:09:34 +0100

    Add test

---
+++ tests/test_mml_pooling.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include <modularml>
+
+const MaxPoolingLayer<float> max_pool(std::vector<int>{2, 2}, std::vector<int>{2, 2});
+const AvgPoolingLayer<float> avg_pool(std::vector<int>{2, 2}, std::vector<int>{2, 2});
+
+TEST(test_mml_pooling, test_max_pooling_1) {
+  const shared_ptr<Tensor<float>> input = tensor_mml<float>({1, 4, 4, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+  const shared_ptr<Tensor<float>> output = tensor_mml<float>({1, 2, 2, 1}, {6, 8, 14, 16});
+
+  Tensor<float> result = max_pool.forward(*input);
+
+  ASSERT_EQ((result), (*output));
+}
