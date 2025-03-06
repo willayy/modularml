9f2d05950d507132274b7ecb0bfca73911f09059
Breman402 <mans.bremer@gmail.com>
2025-03-06 13:22:54 +0100

    Add test for relu int32

--- tests/test_nodes.cpp
+++ tests/test_nodes.cpp
@@ -18,3 +18,20 @@ TEST(test_node, test_ReLU_float) {
   auto& result = get<Tensor_mml<float>>(*Y);
   ASSERT_EQ(result, *b);
 }
+
+TEST(test_node, test_ReLU_int32) {
+  /**
+   * @brief Expected Tensor after the ReLU function is applied to each element.
+   */
+  auto b = tensor_mml_p<int32_t>({3, 3}, {0, 5, 0, 10, 0, 15, 20, 0, 25});
+
+  auto X = make_shared<DataTypes>(Tensor_mml<int32_t>({3, 3}, {-7, 5, -3, 10, -2, 15, 20, -6, 25}));
+  auto Y = make_shared<DataTypes>(Tensor_mml<int32_t>({3, 3}));
+
+  ReLUNode reluNode(X, Y);
+  reluNode.forward();
+
+  // Retrieve the tensor from the shared pointer Y
+  auto& result = get<Tensor_mml<int32_t>>(*Y);
+  ASSERT_EQ(result, *b);
+}
