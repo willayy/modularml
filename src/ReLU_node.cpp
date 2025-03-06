42f70853d5cbbfb4afe6dba6fab20b491f1556a2
Breman402 <mans.bremer@gmail.com>
2025-03-06 13:16:49 +0100

    Remove non-needed if statement

--- src/ReLU_node.cpp
+++ src/ReLU_node.cpp
@@ -9,18 +9,11 @@ void ReLUNode::forward() {
 
   visit([this](auto &x_tensor) {
     using T = typename remove_reference_t<decltype(x_tensor)>::value_type;
-
-    auto shapeX = x_tensor.get_shape();
-    if (shapeX.size() < 2)
-      throw runtime_error("Tensor X must be at least 2D.");
     auto X_ptr = make_shared<Tensor_mml<T>>(x_tensor);
-
     Arithmetic_mml<T> arithmetic;
-
     arithmetic.elementwise_in_place(X_ptr, [](T x) { return x > 0 ? x : 0; });
-
     // Update the output tensor.
-    Y->template emplace<Tensor_mml<T>>(*X_ptr);
+    Y->emplace<Tensor_mml<T>>(*X_ptr);
   },
         *X);
 }
