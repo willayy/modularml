907b2db9a1f2356748ca58b7618c1b1493d2ff8c
Breman402 <mans.bremer@gmail.com>
2025-03-06 20:07:57 +0100

    Make elementwise functions support any tensor dimension

--- src/include/mml_arithmetic.hpp
+++ src/include/mml_arithmetic.hpp
@@ -55,25 +55,53 @@ class Arithmetic_mml : public ArithmeticModule<T> {
   }
 
   void elementwise(const shared_ptr<Tensor<T>> a, T (*f)(T), const shared_ptr<Tensor<T>> c) const override {
-    const auto rows = a->get_shape()[0];
-    const auto cols = a->get_shape()[1];
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        (*c)[{i, j}] = f((*a)[{i, j}]);
+    const auto shape = a->get_shape();
+    const auto num_dimensions = shape.size();
+
+    array_mml<int> indices(num_dimensions);
+    for (uint64_t i = 0; i < num_dimensions; ++i) {
+      indices[i] = 0;
+    }
+    const auto total_elements = a->get_size();
+
+    for (int linear_idx = 0; linear_idx < total_elements; ++linear_idx) {
+      // Apply function `f` from `a` to `c`
+      (*c)[indices] = f((*a)[indices]);
+
+      // Increment indices like a multi-dimensional counter
+      for (int d = num_dimensions - 1; d >= 0; --d) {
+        if (++indices[d] < shape[d]) {
+          break;  // No carry needed, continue iteration
+        }
+        indices[d] = 0;  // Carry over to the next dimension
       }
     }
   }
 
   void elementwise_in_place(const shared_ptr<Tensor<T>> a, T (*f)(T)) const override {
-    const auto rows = a->get_shape()[0];
-    const auto cols = a->get_shape()[1];
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        (*a)[{i, j}] = f((*a)[{i, j}]);
+    const auto shape = a->get_shape();
+    const auto num_dimensions = shape.size();
+
+    array_mml<int> indices(num_dimensions);
+    for (uint64_t i = 0; i < num_dimensions; ++i) {
+      indices[i] = 0;
+    }
+
+    const auto total_elements = a->get_size();
+
+    for (int linear_idx = 0; linear_idx < total_elements; ++linear_idx) {
+      // Apply the function `f` to the current element
+      (*a)[indices] = f((*a)[indices]);
+
+      // Increment indices like a multi-dimensional counter
+      for (int d = num_dimensions - 1; d >= 0; --d) {
+        if (++indices[d] < shape[d]) {
+          break;  // No carry needed, move to the next iteration
+        }
+        indices[d] = 0;  // Carry over to the next dimension
       }
     }
   }
-
   shared_ptr<ArithmeticModule<T>> clone() const override {
     return make_shared<Arithmetic_mml>(*this);
   }
