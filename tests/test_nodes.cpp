e0a6576d11720f740f02e4b505d14deaf48eefb0
Breman402 <mans.bremer@gmail.com>
2025-03-06 16:45:34 +0100

    Add TanH node

--- tests/test_nodes.cpp
+++ tests/test_nodes.cpp
@@ -34,3 +34,20 @@ TEST(test_node, test_ReLU_int32) {
   auto& result = get<Tensor_mml<int32_t>>(*Y);
   ASSERT_EQ(result, *b);
 }
+
+TEST(test_node, test_TanH_float){
+  /**
+   * @brief Expected Tensor after the ReLU function is applied to each element.
+   */
+  auto b = tensor_mml_p<float>({3, 3}, {-0.7615941559557649f, 0.0f, 0.7615941559557649f, -0.9640275800758169f, 0.9640275800758169f, -0.9950547536867305f, 0.9950547536867305f, 0.999329299739067f, -0.999329299739067f});
+
+  auto X = make_shared<TanHNode::DataTypes>(Tensor_mml<float>({3, 3}, {-1.0f, 0.0f, 1.0f, -2.0f, 2.0f, -3.0f, 3.0f, 4.0f, -4.0f}));
+  auto Y = make_shared<TanHNode::DataTypes>(Tensor_mml<float>({3, 3}));
+
+  TanHNode tanhNode(X, Y);
+  tanhNode.forward();
+
+  // Retrieve the tensor from the shared pointer Y
+  auto& result = get<Tensor_mml<float>>(*Y);
+  ASSERT_EQ(result, *b);
+}
