8f0e499ae2186510dd67930122a69a90b45a93da
sanderc1 <sanderjcarlsson@gmail.com>
2025-03-06 18:59:19 +0100

    Bug fix, padding should work

--- src/mml_pooling_layer.tpp
+++ src/mml_pooling_layer.tpp
@@ -25,18 +25,20 @@ shared_ptr<Tensor<T>> PoolingLayer<T>::forward(const shared_ptr<Tensor<T>> t) co
   if (shape.size() != 4) {
     throw std::invalid_argument("Invalid tensor shape");
   } else {
-    int pad_h = 0;
-    int pad_w = 0;
+    float pad_h = 0;
+    float pad_w = 0;
     if (padding == "same") {
-      pad_h = std::max(0, (filter[0] - 1) / 2);
-      pad_w = std::max(0, (filter[1] - 1) / 2);
+      pad_h = static_cast<float>(filter[0] - 1) / 2;
+      pad_w = static_cast<float>(filter[1] - 1) / 2;
     }
 
     // Calculate output dimensions
-    int padded_height = shape[1] + 2 * pad_h;
-    int padded_width = shape[2] + 2 * pad_w;
+    int padded_height = shape[1] + static_cast<int>(2 * pad_h);
+    int padded_width = shape[2] + static_cast<int>(2 * pad_w);
     int output_height = (padded_height - filter[0]) / stride[0] + 1;
     int output_width = (padded_width - filter[1]) / stride[1] + 1;
+
+    std::cerr << "Padded height: " << padded_height << " Padded width: " << padded_width << "\n";
 
     /// Initialize output tensor with correct dimensions
     shared_ptr<Tensor<T>> output_tensor = tensor_mml_p<T>({shape[0], output_height, output_width, shape[3]});
@@ -50,16 +52,15 @@ shared_ptr<Tensor<T>> PoolingLayer<T>::forward(const shared_ptr<Tensor<T>> t) co
           /// Fourth for loop. Each output column
           for (int out_col = 0; out_col < output_width; out_col++) {
             // Calculate input region start (with stride)
-            int in_row_start = out_row * stride[0] - pad_h;
-            int in_col_start = out_col * stride[1] - pad_w;
+            int in_row_start = out_row * stride[0] - static_cast<int>(std::floor(pad_h));
+            int in_col_start = out_col * stride[1] - static_cast<int>(std::floor(pad_w));
             /// Initialize lowest value for type T
             T value = pooling(t, shape, element, channel, in_row_start, in_col_start);
 
             // Store result in output tensor
             if (element < 0 || out_row < 0 || out_col < 0 || channel < 0 || element >= shape[0] || out_row >= output_height || out_col >= output_width || channel >= shape[3]) {
-              throw std::out_of_range("FUCK");
+              throw std::out_of_range("Output tensor indices out of range");
             } else {
-              std::cerr << "element: " << element << " row: " << out_row << " col: " << out_col << " channel: " << channel << "\n";
               (*output_tensor)[{element, out_row, out_col, channel}] = value;
             }
           }
@@ -67,6 +68,8 @@ shared_ptr<Tensor<T>> PoolingLayer<T>::forward(const shared_ptr<Tensor<T>> t) co
       }
     }
     /// TODO discuss return type
+    std::cerr << "Resulting tensor: " << (*output_tensor).to_string() << "\n";
+
     return output_tensor;
   }
 }
--- tests/test_mml_pooling.cpp
+++ tests/test_mml_pooling.cpp
@@ -2,20 +2,62 @@
 
 #include <modularml>
 
-const MaxPoolingLayer<float> max_pool(std::vector<int>{2, 2}, std::vector<int>{2, 2});
-const AvgPoolingLayer<float> avg_pool(std::vector<int>{2, 2}, std::vector<int>{2, 2});
+const MaxPoolingLayer<float> max_pool_without_padding(std::vector<int>{2, 2}, std::vector<int>{2, 2});
+const AvgPoolingLayer<float> avg_pool_without_padding(std::vector<int>{2, 2}, std::vector<int>{2, 2});
 
-TEST(test_mml_pooling, test_max_pooling_1) {
-  shared_ptr<Tensor<float>> t = tensor_mml_p<float>({1, 4, 4, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-  float value = (*t)[{0, 0, 0, 0}];
-  ASSERT_EQ(value, 1);
-}
+const MaxPoolingLayer<float> max_pool_with_padding(std::vector<int>{2, 2}, std::vector<int>{2, 2}, "same");
+const AvgPoolingLayer<float> avg_pool_with_padding(std::vector<int>{2, 2}, std::vector<int>{2, 2}, "same");
 
-TEST(test_mml_pooling, test_max_pooling_2) {
+TEST(test_mml_pooling, test_max_pooling_without_padding_1) {
   const shared_ptr<Tensor<float>> input = tensor_mml_p<float>({1, 4, 4, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
   const shared_ptr<Tensor<float>> output = tensor_mml_p<float>({1, 2, 2, 1}, {6, 8, 14, 16});
 
-  shared_ptr<Tensor<float>> result = max_pool.forward(input);
+  shared_ptr<Tensor<float>> result = max_pool_without_padding.forward(input);
+
+  ASSERT_EQ((*result), (*output));
+}
+
+TEST(test_mml_pooling, test_max_pooling_with_padding_1) {
+  const shared_ptr<Tensor<float>> input = tensor_mml_p<float>({1, 4, 4, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+  const shared_ptr<Tensor<float>> output = tensor_mml_p<float>({1, 2, 2, 1}, {6, 8, 14, 16});
+
+  shared_ptr<Tensor<float>> result = max_pool_with_padding.forward(input);
+
+  ASSERT_EQ((*result), (*output));
+}
+
+TEST(test_mml_pooling, test_avg_pooling_with_padding_1) {
+  const shared_ptr<Tensor<float>> input = tensor_mml_p<float>({1, 4, 4, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+  const shared_ptr<Tensor<float>> output = tensor_mml_p<float>({1, 2, 2, 1}, {3.5, 5.5, 11.5, 13.5});
+
+  shared_ptr<Tensor<float>> result = avg_pool_without_padding.forward(input);
+
+  ASSERT_EQ((*result), (*output));
+}
+
+TEST(test_mml_pooling, test_avg_pooling_without_padding_1) {
+  const shared_ptr<Tensor<float>> input = tensor_mml_p<float>({1, 4, 4, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+  const shared_ptr<Tensor<float>> output = tensor_mml_p<float>({1, 2, 2, 1}, {3.5, 5.5, 11.5, 13.5});
+
+  shared_ptr<Tensor<float>> result = avg_pool_with_padding.forward(input);
+
+  ASSERT_EQ((*result), (*output));
+}
+
+TEST(test_mml_pooling, test_max_pooling_with_padding_2) {
+  const shared_ptr<Tensor<float>> input = tensor_mml_p<float>({1, 4, 5, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
+  const shared_ptr<Tensor<float>> output = tensor_mml_p<float>({1, 2, 3, 1}, {7, 9, 10, 17, 19, 20});
+
+  shared_ptr<Tensor<float>> result = max_pool_with_padding.forward(input);
+
+  ASSERT_EQ((*result), (*output));
+}
+
+TEST(test_mml_pooling, test_avg_pooling_with_padding_2) {
+  const shared_ptr<Tensor<float>> input = tensor_mml_p<float>({1, 4, 5, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
+  const shared_ptr<Tensor<float>> output = tensor_mml_p<float>({1, 2, 3, 1}, {4.0, 6.0, 3.75, 14.0, 16.0, 8.75});
+
+  shared_ptr<Tensor<float>> result = avg_pool_with_padding.forward(input);
 
   ASSERT_EQ((*result), (*output));
 }
