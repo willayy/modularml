02433b3b1a4de7356d7bf94f88c5179a1cad78df
Breman402 <mans.bremer@gmail.com>
2025-03-06 11:39:40 +0100

    Formating

--- src/include/ReLU_node.hpp
+++ src/include/ReLU_node.hpp
@@ -9,8 +9,7 @@ using DataTypes = variant<
     Tensor_mml<float>,
     Tensor_mml<double>,
     Tensor_mml<int32_t>,
-    Tensor_mml<int64_t>
->;
+    Tensor_mml<int64_t>>;
 
 /**
  * @class ReLUNode
@@ -20,92 +19,90 @@ using DataTypes = variant<
  * in a computational graph. It performs the forward pass computation using the GEMM inner product.
  */
 class ReLUNode : public Node {
-public:
-    /**
-     * @brief Constructor for ReLUNode.
-     *
-     * @param X Shared pointer to the tensor A.
-     * @param Y Shared pointer to the output tensor.
-     */
-    ReLUNode(shared_ptr<DataTypes> X, shared_ptr<DataTypes> Y)
-        : X(X), Y(Y) {}
+ public:
+  /**
+   * @brief Constructor for ReLUNode.
+   *
+   * @param X Shared pointer to the tensor A.
+   * @param Y Shared pointer to the output tensor.
+   */
+  ReLUNode(shared_ptr<DataTypes> X, shared_ptr<DataTypes> Y)
+      : X(X), Y(Y) {}
 
+  /**
+   * @brief Perform the forward pass computation using ReLU activation function.
+   *
+   * This function performs the forward pass computation using the General Matrix Multiply (GEMM) inner product.
+   */
+  void forward() override;
 
-    /**
-     * @brief Perform the forward pass computation using ReLU activation function.
-     *
-     * This function performs the forward pass computation using the General Matrix Multiply (GEMM) inner product.
-     */
-    void forward() override;
-    
-    /**
-     * @brief Check if the input(s) are filled.
-     * 
-     * @return True if the input(s) are filled, false otherwise.
-     */
-    bool areInputsFilled() const override {
-        return X && visit([](const auto& t) { return t.get_size() > 0; }, *X);
-    }
+  /**
+   * @brief Check if the input(s) are filled.
+   *
+   * @return True if the input(s) are filled, false otherwise.
+   */
+  bool areInputsFilled() const override {
+    return X && visit([](const auto& t) { return t.get_size() > 0; }, *X);
+  }
 
-    /**
-     * @brief Set the input(s) for the node.
-     * 
-     * @param inputs The input data to be set, where A is inputs[0].
-     */
-    void setInputs(const vector<GeneralDataTypes>& inputs) override {
-        if (inputs.size() < 1)
-            throw runtime_error("ReLUNode expects at least one input: A.");
-    
-        // Deduce type from the first input.
-        visit([this, &inputs](const auto& tensorA) {
-            using T = typename remove_reference_t<decltype(tensorA)>::value_type;
-    
-            // Restrict T to allowed types.
-            if constexpr (!(std::is_same_v<T, float>   ||
-                            std::is_same_v<T, double>  ||
-                            std::is_same_v<T, int32_t> ||
-                            std::is_same_v<T, int64_t>))
-            {
-                throw runtime_error("ReLUNode input type not supported. Make sure that the input is not unsigned.");
-            } else {
-                try {
-                    auto valueX = std::get<Tensor_mml<T>>(inputs[0]);
+  /**
+   * @brief Set the input(s) for the node.
+   *
+   * @param inputs The input data to be set, where A is inputs[0].
+   */
+  void setInputs(const vector<GeneralDataTypes>& inputs) override {
+    if (inputs.size() < 1)
+      throw runtime_error("ReLUNode expects at least one input: A.");
 
-                    X->template emplace<Tensor_mml<T>>(valueX);
-                    
-                } catch (const std::bad_variant_access&) {
-                    throw runtime_error("Data type mismatch: All inputs must have the same type as X.");
-                }
-            }
-        }, inputs[0]);
-    }
+    // Deduce type from the first input.
+    visit([this, &inputs](const auto& tensorA) {
+      using T = typename remove_reference_t<decltype(tensorA)>::value_type;
 
-    /**
-     * @brief Check if the output(s) are filled.
-     * 
-     * @return True if the output(s) are filled, false otherwise.
-     */
-    bool areOutputsFilled() const override {
-        return Y && visit([](const auto& t) { return t.get_size() > 0; }, *Y);
-    }
+      // Restrict T to allowed types.
+      if constexpr (!(std::is_same_v<T, float> ||
+                      std::is_same_v<T, double> ||
+                      std::is_same_v<T, int32_t> ||
+                      std::is_same_v<T, int64_t>)) {
+        throw runtime_error("ReLUNode input type not supported. Make sure that the input is not unsigned.");
+      } else {
+        try {
+          auto valueX = std::get<Tensor_mml<T>>(inputs[0]);
 
-    /**
-     * @brief Get the output of the node.
-     * 
-     * @return The output data.
-     */
-    vector<GeneralDataTypes> getOutputs() const override {
-        if (!Y) {
-            throw runtime_error("Output tensor Y is not filled!");
+          X->template emplace<Tensor_mml<T>>(valueX);
+
+        } catch (const std::bad_variant_access&) {
+          throw runtime_error("Data type mismatch: All inputs must have the same type as X.");
         }
-        return { visit([](const auto& arg) -> GeneralDataTypes { return arg; }, *Y) };
+      }
+    },
+          inputs[0]);
+  }
+
+  /**
+   * @brief Check if the output(s) are filled.
+   *
+   * @return True if the output(s) are filled, false otherwise.
+   */
+  bool areOutputsFilled() const override {
+    return Y && visit([](const auto& t) { return t.get_size() > 0; }, *Y);
+  }
+
+  /**
+   * @brief Get the output of the node.
+   *
+   * @return The output data.
+   */
+  vector<GeneralDataTypes> getOutputs() const override {
+    if (!Y) {
+      throw runtime_error("Output tensor Y is not filled!");
     }
+    return {visit([](const auto& arg) -> GeneralDataTypes { return arg; }, *Y)};
+  }
 
-private:
-    // Inputs
-    shared_ptr<DataTypes> X; // Input tensor X.
+ private:
+  // Inputs
+  shared_ptr<DataTypes> X;  // Input tensor X.
 
-    // Output
-    shared_ptr<DataTypes> Y; // Output tensor.
-    
+  // Output
+  shared_ptr<DataTypes> Y;  // Output tensor.
 };
