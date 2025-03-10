c5a1b55c3bbcc32ccb64995a3dca6ad59dc5f1fd
William Norland <williamnorland@gmail.com>
2025-03-10 19:05:13 +0100

    Format new files

--- src/include/a_onnx_gemm.hpp
+++ src/include/a_onnx_gemm.hpp
@@ -40,10 +40,11 @@ class OnnxGemmModule {
    * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
    * @return Output tensor Y. Output tensor of shape (M, N).
    */
-  virtual shared_ptr<Tensor<T>> gemm_inner_product(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                                   shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
+  virtual shared_ptr<Tensor<T>> gemm_inner_product(float alpha = 1.0, float beta = 1.0,
+                                                   int transA = 0, int transB = 0,
+                                                   shared_ptr<Tensor<T>> A,
+                                                   shared_ptr<Tensor<T>> B,
                                                    optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
-
 
   /**
    * @brief Basic CPU implementation of GEMM, with the outer product approach.
@@ -59,116 +60,129 @@ class OnnxGemmModule {
    * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
    * @return Output tensor Y. Output tensor of shape (M, N).
    */
-    virtual shared_ptr<Tensor<T>> gemm_outer_product(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                                     shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
-                                                     optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
+  virtual shared_ptr<Tensor<T>> gemm_outer_product(float alpha = 1.0, float beta = 1.0,
+                                                   int transA = 0, int transB = 0,
+                                                   shared_ptr<Tensor<T>> A,
+                                                   shared_ptr<Tensor<T>> B,
+                                                   optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
 
-    /**
-     * @brief Basic CPU implementation of GEMM, with the row-wise product approach.
-     * Performs operation Y := alpha * A * B + beta * C (optional)
-     * More detailed info in
-     * https://onnx.ai/onnx/operators/onnx__Gemm.html
-     * @param alpha Float alpha.
-     * @param beta Float beta.
-     * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
-     * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
-     * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
-     * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
-     * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
-     * @return Output tensor Y. Output tensor of shape (M, N).
-     */
-    virtual shared_ptr<Tensor<T>> gemm_row_wise_product(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                                        shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
-                                                        optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
+  /**
+   * @brief Basic CPU implementation of GEMM, with the row-wise product approach.
+   * Performs operation Y := alpha * A * B + beta * C (optional)
+   * More detailed info in
+   * https://onnx.ai/onnx/operators/onnx__Gemm.html
+   * @param alpha Float alpha.
+   * @param beta Float beta.
+   * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
+   * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
+   * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
+   * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
+   * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
+   * @return Output tensor Y. Output tensor of shape (M, N).
+   */
+  virtual shared_ptr<Tensor<T>> gemm_row_wise_product(float alpha = 1.0, float beta = 1.0,
+                                                      int transA = 0, int transB = 0,
+                                                      shared_ptr<Tensor<T>> A,
+                                                      shared_ptr<Tensor<T>> B,
+                                                      optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
 
-    /**
-     * @brief Basic CPU implementation of GEMM, with the col-wise product approach.
-     * Performs operation Y := alpha * A * B + beta * C (optional)
-     * More detailed info in
-     * https://onnx.ai/onnx/operators/onnx__Gemm.html
-     * @param alpha Float alpha.
-     * @param beta Float beta.
-     * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
-     * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
-     * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
-     * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
-     * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
-     * @return Output tensor Y. Output tensor of shape (M, N).
-     */
-    virtual shared_ptr<Tensor<T>> gemm_col_wise_product(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                                        shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
-                                                        optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
+  /**
+   * @brief Basic CPU implementation of GEMM, with the col-wise product approach.
+   * Performs operation Y := alpha * A * B + beta * C (optional)
+   * More detailed info in
+   * https://onnx.ai/onnx/operators/onnx__Gemm.html
+   * @param alpha Float alpha.
+   * @param beta Float beta.
+   * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
+   * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
+   * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
+   * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
+   * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
+   * @return Output tensor Y. Output tensor of shape (M, N).
+   */
+  virtual shared_ptr<Tensor<T>> gemm_col_wise_product(float alpha = 1.0, float beta = 1.0,
+                                                      int transA = 0, int transB = 0,
+                                                      shared_ptr<Tensor<T>> A,
+                                                      shared_ptr<Tensor<T>> B,
+                                                      optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
 
-    /**
-     * @brief Blocked CPU implementation of GEMM.
-     * Performs operation Y := alpha * A * B + beta * C (optional)
-     * More detailed info in
-     * https://onnx.ai/onnx/operators/onnx__Gemm.html
-     * @param alpha Float alpha.
-     * @param beta Float beta.
-     * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
-     * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
-     * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
-     * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
-     * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
-     * @return Output tensor Y. Output tensor of shape (M, N).
-     */
-    virtual shared_ptr<Tensor<T>> gemm_blocked(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                               shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
+  /**
+   * @brief Blocked CPU implementation of GEMM.
+   * Performs operation Y := alpha * A * B + beta * C (optional)
+   * More detailed info in
+   * https://onnx.ai/onnx/operators/onnx__Gemm.html
+   * @param alpha Float alpha.
+   * @param beta Float beta.
+   * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
+   * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
+   * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
+   * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
+   * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
+   * @return Output tensor Y. Output tensor of shape (M, N).
+   */
+  virtual shared_ptr<Tensor<T>> gemm_blocked(float alpha = 1.0, float beta = 1.0,
+                                             int transA = 0, int transB = 0,
+                                             shared_ptr<Tensor<T>> A,
+                                             shared_ptr<Tensor<T>> B,
+                                             optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
+
+  /**
+   * @brief Vectorized implementation of GEMM using SIMD thanks to AVX.
+   * Performs operation Y := alpha * A * B + beta * C (optional)
+   * More detailed info in
+   * https://onnx.ai/onnx/operators/onnx__Gemm.html
+   * @param alpha Float alpha.
+   * @param beta Float beta.
+   * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
+   * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
+   * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
+   * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
+   * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
+   * @return Output tensor Y. Output tensor of shape (M, N).
+   */
+  virtual shared_ptr<Tensor<T>> gemm_avx(float alpha = 1.0, float beta = 1.0,
+                                         int transA = 0, int transB = 0,
+                                         shared_ptr<Tensor<T>> A,
+                                         shared_ptr<Tensor<T>> B,
+                                         optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
+
+  /**
+   * @brief Vectorized implementation of GEMM using SIMD thanks to AVX512.
+   * Performs operation Y := alpha * A * B + beta * C (optional)
+   * More detailed info in
+   * https://onnx.ai/onnx/operators/onnx__Gemm.html
+   * @param alpha Float alpha.
+   * @param beta Float beta.
+   * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
+   * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
+   * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
+   * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
+   * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
+   * @return Output tensor Y. Output tensor of shape (M, N).
+   */
+  virtual shared_ptr<Tensor<T>> gemm_avx512(float alpha = 1.0, float beta = 1.0,
+                                            int transA = 0, int transB = 0,
+                                            shared_ptr<Tensor<T>> A,
+                                            shared_ptr<Tensor<T>> B,
+                                            optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
+
+  /**
+   * @brief GEMM using Intel's Math Kernel Library.
+   * Performs operation Y := alpha * A * B + beta * C (optional)
+   * More detailed info in
+   * https://onnx.ai/onnx/operators/onnx__Gemm.html
+   * @param alpha Float alpha.
+   * @param beta Float beta.
+   * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
+   * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
+   * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
+   * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
+   * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
+   * @return Output tensor Y. Output tensor of shape (M, N).
+   */
+  virtual shared_ptr<Tensor<T>> gemm_intel_MKL(float alpha = 1.0, float beta = 1.0,
+                                               int transA = 0, int transB = 0,
+                                               shared_ptr<Tensor<T>> A,
+                                               shared_ptr<Tensor<T>> B,
                                                optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
-
-    /**
-     * @brief Vectorized implementation of GEMM using SIMD thanks to AVX.
-     * Performs operation Y := alpha * A * B + beta * C (optional)
-     * More detailed info in
-     * https://onnx.ai/onnx/operators/onnx__Gemm.html
-     * @param alpha Float alpha.
-     * @param beta Float beta.
-     * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
-     * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
-     * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
-     * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
-     * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
-     * @return Output tensor Y. Output tensor of shape (M, N).
-     */
-    virtual shared_ptr<Tensor<T>> gemm_avx(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                           shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
-                                           optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
-
-    /**
-     * @brief Vectorized implementation of GEMM using SIMD thanks to AVX512.
-     * Performs operation Y := alpha * A * B + beta * C (optional)
-     * More detailed info in
-     * https://onnx.ai/onnx/operators/onnx__Gemm.html
-     * @param alpha Float alpha.
-     * @param beta Float beta.
-     * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
-     * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
-     * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
-     * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
-     * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
-     * @return Output tensor Y. Output tensor of shape (M, N).
-     */
-    virtual shared_ptr<Tensor<T>> gemm_avx512(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                              shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
-                                              optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
-
-    /**
-     * @brief GEMM using Intel's Math Kernel Library.
-     * Performs operation Y := alpha * A * B + beta * C (optional)
-     * More detailed info in
-     * https://onnx.ai/onnx/operators/onnx__Gemm.html
-     * @param alpha Float alpha.
-     * @param beta Float beta.
-     * @param transA 0 if matrix A is not transposed, 1 if matrix A is transposed
-     * @param transB 0 if matrix B is not transposed, 1 if matrix B is transposed
-     * @param A Input tensor A. The shape of A should be (M, K) if transA is 0, or (K, M) if transA is non-zero.
-     * @param B Input tensor B. The shape of B should be (K, N) if transB is 0, or (N, K) if transB is non-zero.
-     * @param C Optional input tensor C. If not specified, the computation is done as if C is a scalar 0. The shape of C should be unidirectional broadcastable to (M, N).
-     * @return Output tensor Y. Output tensor of shape (M, N).
-     */
-    virtual shared_ptr<Tensor<T>> gemm_intel_MKL(float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0,
-                                                 shared_ptr<Tensor<T>> A, shared_ptr<Tensor<T>> B,
-                                                 optional<shared_ptr<Tensor<T>>> C = nullopt) = 0;
-
 };
--- src/mml_gemm.tpp
+++ src/mml_gemm.tpp
@@ -4,38 +4,38 @@
 
 template <typename T>
 void Gemm_mml<T>::gemm_inner_product(int TA, int TB, int M, int N, int K, T ALPHA,
-                                  shared_ptr<Tensor<T>> A, int lda,
-                                  shared_ptr<Tensor<T>> B, int ldb,
-                                  T BETA,
-                                  shared_ptr<Tensor<T>> C, int ldc) {
-if (!TA && !TB) {
-  int i, j, k;
-  int i_col, k_col, i_col_out;
+                                     shared_ptr<Tensor<T>> A, int lda,
+                                     shared_ptr<Tensor<T>> B, int ldb,
+                                     T BETA,
+                                     shared_ptr<Tensor<T>> C, int ldc) {
+  if (!TA && !TB) {
+    int i, j, k;
+    int i_col, k_col, i_col_out;
 
-  for (i = 0; i < M; i++) {
-    i_col_out = i * ldc;
-    for (j = 0; j < N; j++) {
-      (*C)[i_col_out + j] = ((T)BETA) * (*C)[i_col_out + j];
-    }
-    for (k = 0; k < K; k++) {
-      k_col = k * ldb;
+    for (i = 0; i < M; i++) {
+      i_col_out = i * ldc;
       for (j = 0; j < N; j++) {
-        (*C)[i_col_out + j] += ((T)ALPHA) * (*A)[i * lda + k] * (*B)[k_col + j];
+        (*C)[i_col_out + j] = ((T)BETA) * (*C)[i_col_out + j];
+      }
+      for (k = 0; k < K; k++) {
+        k_col = k * ldb;
+        for (j = 0; j < N; j++) {
+          (*C)[i_col_out + j] += ((T)ALPHA) * (*A)[i * lda + k] * (*B)[k_col + j];
+        }
       }
     }
+  } else {
+    logic_error("Transposition not yet supported in GEMM inner product.");
   }
-} else {
-  logic_error("Transposition not yet supported in GEMM inner product.");
-}
-return;
+  return;
 }
 
 template <typename T>
 void Gemm_mml<T>::gemm_outer_product(int TA, int TB, int M, int N, int K, T ALPHA,
-                        shared_ptr<Tensor<T>> A, int lda,
-                        shared_ptr<Tensor<T>> B, int ldb,
-                        T BETA,
-                        shared_ptr<Tensor<T>> C, int ldc) {
+                                     shared_ptr<Tensor<T>> A, int lda,
+                                     shared_ptr<Tensor<T>> B, int ldb,
+                                     T BETA,
+                                     shared_ptr<Tensor<T>> C, int ldc) {
   if (!TA && !TB) {
     int i, j, k;
     int i_col, k_col, i_col_out;
@@ -65,10 +65,10 @@ void Gemm_mml<T>::gemm_outer_product(int TA, int TB, int M, int N, int K, T ALPH
 
 template <typename T>
 void Gemm_mml<T>::gemm_row_wise_product(int TA, int TB, int M, int N, int K, T ALPHA,
-                          shared_ptr<Tensor<T>> A, int lda,
-                          shared_ptr<Tensor<T>> B, int ldb,
-                          T BETA,
-                          shared_ptr<Tensor<T>> C, int ldc) {
+                                        shared_ptr<Tensor<T>> A, int lda,
+                                        shared_ptr<Tensor<T>> B, int ldb,
+                                        T BETA,
+                                        shared_ptr<Tensor<T>> C, int ldc) {
   if (!TA && !TB) {
     int i, j, k;
     int i_col, k_col, i_col_out;
@@ -94,10 +94,10 @@ void Gemm_mml<T>::gemm_row_wise_product(int TA, int TB, int M, int N, int K, T A
 
 template <typename T>
 void Gemm_mml<T>::gemm_col_wise_product(int TA, int TB, int M, int N, int K, T ALPHA,
-                          shared_ptr<Tensor<T>> A, int lda,
-                          shared_ptr<Tensor<T>> B, int ldb,
-                          T BETA,
-                          shared_ptr<Tensor<T>> C, int ldc) {
+                                        shared_ptr<Tensor<T>> A, int lda,
+                                        shared_ptr<Tensor<T>> B, int ldb,
+                                        T BETA,
+                                        shared_ptr<Tensor<T>> C, int ldc) {
   if (!TA && !TB) {
     int i, j, k;
     int i_col, k_col, i_col_out;
@@ -125,35 +125,33 @@ void Gemm_mml<T>::gemm_col_wise_product(int TA, int TB, int M, int N, int K, T A
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 template <typename T>
 void Gemm_mml<T>::gemm_blocked(int TA, int TB, int M, int N, int K, T ALPHA,
-                  shared_ptr<Tensor<T>> A, int lda,
-                  shared_ptr<Tensor<T>> B, int ldb,
-                  T BETA,
-                  shared_ptr<Tensor<T>> C, int ldc) {
+                               shared_ptr<Tensor<T>> A, int lda,
+                               shared_ptr<Tensor<T>> B, int ldb,
+                               T BETA,
+                               shared_ptr<Tensor<T>> C, int ldc) {
   logic_error("Blocked GEMM not yet supported.");
 }
 template <typename T>
 void Gemm_mml<T>::gemm_avx(int TA, int TB, int M, int N, int K, T ALPHA,
-              shared_ptr<Tensor<T>> A, int lda,
-              shared_ptr<Tensor<T>> B, int ldb,
-              T BETA,
-              shared_ptr<Tensor<T>> C, int ldc) {
+                           shared_ptr<Tensor<T>> A, int lda,
+                           shared_ptr<Tensor<T>> B, int ldb,
+                           T BETA,
+                           shared_ptr<Tensor<T>> C, int ldc) {
   logic_error("AVX GEMM not yet supported.");
 }
 template <typename T>
 void Gemm_mml<T>::gemm_avx512(int TA, int TB, int M, int N, int K, T ALPHA,
-                shared_ptr<Tensor<T>> A, int lda,
-                shared_ptr<Tensor<T>> B, int ldb,
-                T BETA,
-                shared_ptr<Tensor<T>> C, int ldc) {
+                              shared_ptr<Tensor<T>> A, int lda,
+                              shared_ptr<Tensor<T>> B, int ldb,
+                              T BETA,
+                              shared_ptr<Tensor<T>> C, int ldc) {
   logic_error("AVX-512 GEMM not yet supported.");
 }
 template <typename T>
 void Gemm_mml<T>::gemm_intel_MKL(int TA, int TB, int M, int N, int K, T ALPHA,
-                    shared_ptr<Tensor<T>> A, int lda,
-                    shared_ptr<Tensor<T>> B, int ldb,
-                    T BETA,
-                    shared_ptr<Tensor<T>> C, int ldc) {
+                                 shared_ptr<Tensor<T>> A, int lda,
+                                 shared_ptr<Tensor<T>> B, int ldb,
+                                 T BETA,
+                                 shared_ptr<Tensor<T>> C, int ldc) {
   logic_error("Intel MKL GEMM not yet supported.");
 }
-
-
