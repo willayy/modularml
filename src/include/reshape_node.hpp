f0d70da742675d1b178193c561dbdc650f2f5d44
Breman402 <mans.bremer@gmail.com>
2025-03-11 17:19:12 +0100

    Implement forward for reshape_node

--- src/include/reshape_node.hpp
+++ src/include/reshape_node.hpp
@@ -3,7 +3,6 @@
 #include "a_node.hpp"
 #include "a_tensor.hpp"
 #include "globals.hpp"
-#include "mml_arithmetic.hpp"
 
 /**
  * @class reshapeNode
@@ -42,6 +41,9 @@ class reshapeNode : public Node {
       throw runtime_error("Invalid value for allowzero. Must be 0 or 1.");
     if (!shape)
       throw runtime_error("Shape tensor must be of type int64_t.");
+    if constexpr (std::is_same_v<T, string> || std::is_same_v<T, bool>) {
+      throw runtime_error("Reshape operation is not yet implemented for string or bool tensors.");
+    }
   }
 
   /**
@@ -60,7 +62,39 @@ class reshapeNode : public Node {
     if (!reshaped)
       throw runtime_error("Output tensor reshaped is not allocated.");
 
-    Arithmetic_mml<T> arithmetic;
+    int shape_size = shape->get_size();
+    int data_size = data->get_size();
+    array_mml<int> new_shape(shape_size);
+
+    // Check if the total number of elements matches between the data tensor and the new shape.
+    int total_elements = 1;
+    for (int i = 0; i < shape_size; ++i) {
+      total_elements *= new_shape[i];
+    }
+
+    if (total_elements != data_size) {
+      throw runtime_error("The total number of elements in the new shape does not match the number of elements in the data tensor.");
+    }
+
+    // Iterate over each dimension in the shape tensor.
+    for (int i = 0; i < shape_size; ++i) {
+      int dim = (*shape)[i];
+      // If the dimension is zero and allowzero is set to 1, use the corresponding dimension from the input tensor.
+      if (dim == 0 && allowzero == 1) {
+        new_shape[i] = data->get_shape()[i];
+      } else {
+        // Otherwise, use the dimension specified in the shape tensor.
+        new_shape[i] = static_cast<int>(dim);
+      }
+    }
+
+    // Reshape the output tensor to the new shape.
+    reshaped->reshape(new_shape);
+
+    // Copy the data from the input tensor to the reshaped output tensor.
+    for (int i = 0; i < data_size; ++i) {
+      (*reshaped)[i] = (*data)[i];
+    }
   }
 
   /**
@@ -75,7 +109,7 @@ class reshapeNode : public Node {
    *
    * @param inputs The input data to be set, where inputs[0] is the data tensor and inputs[1] is the shape tensor.
    */
-  void setInputs(const array_mml<const GeneralDataTypes>& inputs) override {
+  void setInputs(const array_mml<GeneralDataTypes>& inputs) override {
     if (inputs.size() < 2)
       throw runtime_error("reshapeNode expects two inputs: data and shape.");
 
@@ -108,9 +142,9 @@ class reshapeNode : public Node {
 
  private:
   // tensors
-  shared_ptr<const AbstractTensor> data;  // Input tensor data.
-  shared_ptr<const int64_t> shape;        // Input tensor shape.
-  shared_ptr<AbstractTensor> reshaped;    // Output tensor reshaped.
+  shared_ptr<const AbstractTensor> data;    // Input tensor data.
+  shared_ptr<const Tensor<int64_t>> shape;  // Input tensor shape.
+  shared_ptr<AbstractTensor> reshaped;      // Output tensor reshaped.
 
   // attributes
   int allowzero;
