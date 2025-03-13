910bdaf7473c858d6c0b5ada6dfba9864f2813b4
Elias Nilsson <elinil0221@gmail.com>
2025-03-13 15:12:08 +0100

    Add second parser structure

--- src/include/mml_parser.hpp
+++ src/include/mml_parser.hpp
@@ -1,62 +1,149 @@
 #pragma once
 
 #include "a_data_parser.hpp"
+#include "mml_tensor.hpp"
 
-std::unordered_map<std::string, GeneralDataTypes> mapTensors(const json& nodes) {
+
+// Helper function: to map the tensors
+std::unordered_map<std::string, GeneralDataTypes> mapTensors(const json& graph) {
   std::unordered_map<std::string, GeneralDataTypes> tensorMap;
   
   // First look for already initialized inputs
-  for (const auto& node_json: nodes) {
-    if (node_json.contains("initializers") && node_json["initializers"].is_array()) {
-      for (const auto& init: node_json["initializers"]) {
-        std::string initName = init["name"];
-        if (tensorMap.find(initName) != tensorMap.end()) {
-          std::string typeName = init["data_type"];
+  if (graph.contains("initializer") && graph["initializer"].is_array()) {
+    for (const auto& init: graph["initializer"]) {
+      std::string initName = init["name"];
+      int dataType = init["dataType"];
+      
+      std::vector<int> dims;
+      for (const auto& el : init["dims"]) {
+        dims.push_back(std::stoi(el.get<std::string>()));
+      }
+      array_mml shapeArray(dims);
 
-          std::vector<int> shapeVec = init["shape"].get<std::vector<int>>();
-          array_mml<int> shapeArray(shapeVec);
-
-          // For valueArray we should read it from binary in the future!
-          if (typeName == "FLOAT") {
-            auto valueArray = flattenToArrayMml<float>(init["values"]);
-            tensorMap[initName] = std::make_shared<Tensor_mml<float>>(shapeArray,valueArray);
-          } else if (typeName == "DOUBLE") {
-            auto valueArray = flattenToArrayMml<double>(init["values"]);
-            tensorMap[initName] = std::make_shared<Tensor_mml<double>>(shapeArray, valueArray);
-          } else if (typeName == "INT32") {
-            auto valueArray = flattenToArrayMml<int32_t>(init["values"]);
-            tensorMap[initName] = std::make_shared<Tensor_mml<int32_t>>(shapeArray, valueArray);
-          } else if (typeName == "INT64") {
-            auto valueArray = flattenToArrayMml<int64_t>(init["values"]);
-            tensorMap[initName] = std::make_shared<Tensor_mml<int64_t>>(shapeArray, valueArray);
-          } else {
-            // Templates are resolved at compilation so need to use if/else or switch.
-            throw std::runtime_error("Currently supported data type: " + typeName);
-          }
+      
+      if (dataType == 1) {
+        std::vector<float> data = init["floatData"].get<std::vector<float>>();
+        array_mml dataArray(data);
+        tensorMap[initName] = std::make_shared<Tensor_mml<float>>(shapeArray, dataArray);
+      } else if (dataType == 7) {
+        std::vector<int64_t> data;
+        for (const auto& el : init["int64Data"]) {
+          data.push_back(std::stoll(el.get<std::string>()));
         }
+        array_mml dataArray(data);
+        tensorMap[initName] = std::make_shared<Tensor_mml<int64_t>>(shapeArray, dataArray);
+      } else {
+        throw std::runtime_error("Currently unsupported data type: " + std::to_string(dataType));
       }
     }
   }
 
-  // Then we make empty construction for unique inputs/outputs
-  for (const auto& node_json: nodes) {
-    if (node_json.contains("inputs") && node_json["inputs"].is_array()) {
-      for (const auto& input: node_json["inputs"]) {
-        std::string inputName = input["name"];
-        if (tensorMap.find(inputName) != tensorMap.end()) {
-          tensorMap[inputName] = nullptr;
-        }
+  // Then look for inputs
+  if (graph.contains("input") && graph["input"].is_array()) {
+    for (const auto& input: graph["input"]) {
+      std::string inputName = input["name"];
+      int dataType = input["type"]["tensorType"]["elemType"];
+      
+      std::vector<int> dims;
+      for (const auto& dim : input["type"]["tensorType"]["shape"]["dim"]) {
+        dims.push_back(std::stoi(dim["dimValue"].get<std::string>()));
       }
-    }
-    if (node_json.contains("outputs") && node_json["outputs"].is_array()) {
-      for (const auto& output: node_json["outputs"]) {
-        std::string outputName = output["name"];
-        if (tensorMap.find(outputName) != tensorMap.end()) {
-          tensorMap[outputName] = nullptr;
-        }
+      array_mml shapeArray(dims);
+      
+      if (dataType == 1) {
+        tensorMap[inputName] = std::make_shared<Tensor_mml<float>>(shapeArray);
+      } else if (dataType == 7) {
+        tensorMap[inputName] = std::make_shared<Tensor_mml<int64_t>>(shapeArray);
+      } else {
+        throw std::runtime_error("Currently unsupported data type: " + std::to_string(dataType));
       }
     }
   }
+
+  // Then look for outputs
+  if (graph.contains("output") && graph["output"].is_array()) {
+    for (const auto& output: graph["output"]) {
+      std::string outputName = output["name"];
+      int dataType = output["type"]["tensorType"]["elemType"];
+      
+      std::vector<int> dims;
+      for (const auto& dim : output["type"]["tensorType"]["shape"]["dim"]) {
+        dims.push_back(std::stoi(dim["dimValue"].get<std::string>()));
+      }
+      array_mml shapeArray(dims);
+      
+      if (dataType == 1) {
+        tensorMap[outputName] = std::make_shared<Tensor_mml<float>>(shapeArray);
+      } else if (dataType == 7) {
+        tensorMap[outputName] = std::make_shared<Tensor_mml<int64_t>>(shapeArray);
+      } else {
+        throw std::runtime_error("Currently unsupported data type: " + std::to_string(dataType));
+      }
+    }
+  }
+
+  // Then look for valueInfo
+  if (graph.contains("valueInfo") && graph["valueInfo"].is_array()) {
+    for (const auto& valueInfo: graph["valueInfo"]) {
+      std::string valueInfoName = valueInfo["name"];
+      int dataType = valueInfo["type"]["tensorType"]["elemType"];
+      
+      std::vector<int> dims;
+      for (const auto& dim : valueInfo["type"]["tensorType"]["shape"]["dim"]) {
+        dims.push_back(std::stoi(dim["dimValue"].get<std::string>()));
+      }
+      array_mml shapeArray(dims);
+      
+      if (dataType == 1) {
+        tensorMap[valueInfoName] = std::make_shared<Tensor_mml<float>>(shapeArray);
+      } else if (dataType == 7) {
+        tensorMap[valueInfoName] = std::make_shared<Tensor_mml<int64_t>>(shapeArray);
+      } else {
+        throw std::runtime_error("Currently unsupported data type: " + std::to_string(dataType));
+      }
+    }
+  }
+
+  return tensorMap;
+}
+
+// Helper function: to construct the nodes
+std::vector<unique_ptr<Node>> constructNodes(const json& graph, const std::unordered_map<std::string, GeneralDataTypes> tensorMap) {
+  std::vector<unique_ptr<Node>> nodes;
+  
+  // Look for nodes
+  if (graph.contains("node") && graph["node"].is_array()) {
+    for (const auto& node: graph["node"]) {
+      std::string opType = node["opType"];
+      
+      // Get the input tensors
+      std::vector<GeneralDataTypes> inputs;
+      for (const auto& input: node["input"]) {
+        inputs.push_back(tensorMap.at(input.get<std::string>()));
+      }
+      
+      // Get the output tensors
+      std::vector<GeneralDataTypes> outputs;
+      for (const auto& output: node["output"]) {
+        outputs.push_back(tensorMap.at(output.get<std::string>()));
+      }
+      
+      // Create the node
+      if (opType == "Add") {
+        nodes.push_back(std::make_unique<>);
+      } else if (opType == "Sub") {
+        nodes.push_back(std::make_unique<>);
+      } else if (opType == "Mul") {
+        nodes.push_back(std::make_unique<>);
+      } else if (opType == "Div") {
+        nodes.push_back(std::make_unique<>);
+      } else {
+        throw std::runtime_error("Currently unsupported operation type: " + opType);
+      }
+    }
+  }
+
+  return nodes;
 }
 
 class Parser_mml: public DataParser {
@@ -64,11 +151,14 @@ class Parser_mml: public DataParser {
     Parser_mml() = default;
     
     unique_ptr<Model> parse(const json& data) const {
-      vector<unique_ptr<Node>> nodes;
-      if (data.contains("nodes") && data["nodes"].is_array()) {
-        for (const auto& node_json: data["nodes"]) {
-          
-        }
-      }
+      //Get the graph
+      json graph = data["graph"];
+
+      // Get the tensors
+      std::unordered_map<std::string, GeneralDataTypes> tensors = mapTensors(graph);
+
+
+
+
     }
 }
