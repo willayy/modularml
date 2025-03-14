77e624ca3ce7369afe3c5dcc5578e22f7bb06aed
William Norland <williamnorland@gmail.com>
2025-03-14 16:12:31 +0100

    Lint disclaimer in a tensor

--- src/include/a_tensor.hpp
+++ src/include/a_tensor.hpp
@@ -59,7 +59,7 @@ class Tensor {
   /// @brief Check if this tensor is not equal to another tensor.
   /// @param other The tensor to compare with.
   /// @return True if the tensors are not equal, false otherwise.
-  virtual bool operator!=(const Tensor<T> &other) const = 0;
+  virtual bool operator!=(const Tensor<T> &other) const = 0;  // NOSONAR - Fair point but, We choose this to enable different tensor implementation but using the same interface.
 
   /// @brief Get an element from the tensor using multi-dimensional indices.
   /// @param indices A vector of integers representing the indices of the element.
@@ -101,17 +101,17 @@ class Tensor {
   ///@brief Check if this tensor is equal to another tensor.
   ///@param other The tensor to compare with.
   ///@return True if the tensors are equal, false otherwise.*/
-  virtual bool operator==(const Tensor<T> &other) const = 0;
+  virtual bool operator==(const Tensor<T> &other) const = 0;  // NOSONAR - Fair point but, We choose this to enable different tensor implementation but using the same interface.
 
   ///@brief Move-Assignment operator.
   ///@param other The tensor to assign.
   ///@return The moved tensor.
-  virtual Tensor &operator=(Tensor &&other) noexcept = 0;
+  virtual Tensor &operator=(Tensor &&other) noexcept = 0;  // NOSONAR - Fair point but, We choose this to enable different tensor implementation but using the same interface.
 
   /// @brief (Deep) Copy-Assigment operator.
   /// @param other The tensor to assign.
   /// @return The copied tensor.
-  virtual Tensor &operator=(const Tensor &other) = 0;
+  virtual Tensor &operator=(const Tensor &other) = 0;  // NOSONAR - Fair point but, We choose this to enable different tensor implementation but using the same interface.
 
   ///@brief Get an element from the tensor using singel-dimensional index.
   ///@param index A single integer representing the index of the element.
