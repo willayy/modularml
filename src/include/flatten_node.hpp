e69e833428ec846eaa988eed957996a433af1b18
Tim <timcarlsson0203@gmail.com>
2025-03-14 15:28:02 +0100

    Add FlattenNode and tests

--- src/include/flatten_node.hpp
+++ src/include/flatten_node.hpp
@@ -12,11 +12,51 @@ public:
     /**
      * @brief Constructor for FlattenNode
      */
-    FlattenNode(shared_ptr)
+    FlattenNode(shared_ptr<AbstractTensor> X,
+                shared_ptr<AbstractTensor> Y,
+                int axis = 1);
 
+
+    /**
+     * @brief
+     */
+    void forward() override;
+
+    /**
+     * @brief Check if the input(s) are filled.
+     *
+     * @return True if the input(s) are filled, false otherwise.
+     */
+    bool areInputsFilled() const override;
+
+    /**
+     * @brief Set the input(s) for the node.
+     *
+     * @param inputs The input data to be set.
+     */
+    void setInputs(const array_mml<GeneralDataTypes>& inputs) override;
+
+    /**
+     * @brief Check if the output(s) are filled.
+     *
+     * @return True if the output(s) are filled, false otherwise.
+     */
+    bool areOutputsFilled() const override;
+
+    /**
+     * @brief Get the output of the node.
+     *
+     * @return The output data.
+     */
+    array_mml<GeneralDataTypes> getOutputs() const override;
 
 private:
-    shared_ptr<Tensor<T>> input;
-    shared_ptr<Tensor<T>> output;
+    shared_ptr<Tensor<T>> X;
+    shared_ptr<Tensor<T>> Y;
     int axis;
+
+
+    int get_axis() const;
 };
+
+#include "../flatten_node.tpp"
---
+++ tests/test_flatten_node.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+
+#include "flatten_node.hpp"
+
+
+TEST(flatten_node_test, test_forward_3d_tensor) {
+    array_mml<int> x_shape({2, 2, 3});
+    array_mml<float> x_values({1.0f, 2.0f, 3.0f,
+                               4.0f, 5.0f, 6.0f,
+                               7.0f, 8.0f, 9.0f, 10.0f, 11.0f});
+
+    shared_ptr<Tensor_mml<float>> X = make_shared<Tensor_mml<float>>(x_shape, x_values);
+
+    // Shape doesnt matter for output
+    array_mml<int> y_shape({1, 1, 1});
+
+    shared_ptr<Tensor_mml<float>> Y = make_shared<Tensor_mml<float>>(y_shape);
+
+
+    FlattenNode<float> flatten(X, Y, 1);
+
+    flatten.forward();
+
+    EXPECT_EQ(Y->get_shape(), array_mml<int>({2, 6}));
+}   
+
+
+TEST(flatten_node_test, test_forward_4d_tensor) {
+    array_mml<int> x_shape({2, 2, 3, 3});
+    array_mml<float> x_values({
+        1.0f, 2.0f, 3.0f,
+        4.0f, 5.0f, 6.0f,
+        7.0f, 8.0f, 9.0f, 10.0f, 11.0f,
+
+        1.0f, 2.0f, 3.0f,
+        4.0f, 5.0f, 6.0f,
+        7.0f, 8.0f, 9.0f, 10.0f, 11.0f,
+
+        1.0f, 2.0f, 3.0f,
+        4.0f, 5.0f, 6.0f,
+        7.0f, 8.0f, 9.0f, 10.0f, 11.0f
+
+    });
+
+    shared_ptr<Tensor_mml<float>> X = make_shared<Tensor_mml<float>>(x_shape, x_values);
+
+    // Shape doesnt matter for output
+    array_mml<int> y_shape({1, 1, 1});
+
+    shared_ptr<Tensor_mml<float>> Y = make_shared<Tensor_mml<float>>(y_shape);
+
+    // Axis = 2 means that that the shape is flattened as such 2x2, 3x3 = {4, 9} 
+    FlattenNode<float> flatten(X, Y, 2);
+
+    flatten.forward();
+
+    EXPECT_EQ(Y->get_shape(), array_mml<int>({4, 9}));
+}   
