9c0ea51507f24a513470ec682a7155358b7b577c
Breman402 <mans.bremer@gmail.com>
2025-03-27 10:54:46 +0100

    Change made by formatter

--- tests/test_mml_arithmetic.cpp
+++ tests/test_mml_arithmetic.cpp
@@ -77,8 +77,8 @@ TEST(test_mml_arithmetic, test_elementwise_in_place) {
 
 TEST(test_mml_arithmetic, test_elementwise_in_place_many_dimensions_4D) {
   const shared_ptr<Tensor<float>> a = tensor_mml_p<float>({2, 2, 3, 2}, 
-    {1.0f,  2.0f,  3.0f,  4.0f,  5.0f,  6.0f,  
-     7.0f,  8.0f,  9.0f, 10.0f, 11.0f, 12.0f,  
+                                                          {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f,
+                                                           7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f,
 
     13.0f, 14.0f, 15.0f, 16.0f, 17.0f, 18.0f,  
     19.0f, 20.0f, 21.0f, 22.0f, 23.0f, 24.0f});
