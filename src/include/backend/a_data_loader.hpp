b4f16b355edf840ba37c12fb71f1d043860845a8
Tim <timcarlsson0203@gmail.com>
2025-04-02 19:17:44 +0200

    Import stb library to load images, add abstract data loader

---
+++ src/include/backend/a_data_loader.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "globals.hpp"
+#include "a_tensor.hpp"
+
+/**
+ * @class DataLoader
+ * @brief Abstract base class for machine learning models.
+ *
+ * This class defines the interface for loading exeternal data and translating it to a Tensor, 
+ * Provides a method for loading data and a virtual destructor.
+ * 
+ * @author Tim Carlsson (timca@chalmers.se)
+ */
+
+template <typename T>
+class DataLoader {
+public:
+    /**
+     * @brief Load external data and translate it to tensor.
+     *
+     * This is a pure virtual function that must be implemented by derived classes.
+     *
+     * @return A unique_ptr to a Tensor containing the data.
+     */
+    virtual unique_ptr<Tensor<T>> load() const = 0;
+
+    /**
+     * @brief Virtual destructor for the DataLoader class.
+     *
+     * Ensures proper cleanup of derived class objects.
+     */
+    virtual ~DataLoader() = default;
+};
