18d9db7b2017aa6949c63413147c3f54e1890c8a
Tim <timcarlsson0203@gmail.com>
2025-04-07 01:53:06 +0200

    Something is wierd

--- tests/test_benchmark_gemm.cpp
+++ tests/test_benchmark_gemm.cpp
@@ -10,7 +10,7 @@ TEST(test_benchmark_gemm, benchmark_gemm_128x128_float) {
     shared_ptr<Tensor<float>> b = TensorFactory::create_tensor<float>({128, 128}, b_data);
     shared_ptr<Tensor<float>> c = TensorFactory::create_tensor<float>({128, 128});
     
-    TensorOperationsModule::gemm<float>(0, 1, 128, 128, 128, 1, a, 128, b, 128, 0, c, 128);
+    TensorOperationsModule::gemm<float>(0, 0, 128, 128, 128, 1, a, 128, b, 128, 0, c, 128);
     
     ASSERT_TRUE(1); // This test is here to be able to check the time it takes for different GEMM inplementations
 }
@@ -24,7 +24,7 @@ TEST(test_benchmark_gemm, benchmark_gemm_256x256_float) {
     shared_ptr<Tensor<float>> b = TensorFactory::create_tensor<float>({256, 256}, b_data);
     shared_ptr<Tensor<float>> c = TensorFactory::create_tensor<float>({256, 256});
     
-    TensorOperationsModule::gemm<float>(0, 1, 256, 256, 256, 1, a, 256, b, 256, 0, c, 256);
+    TensorOperationsModule::gemm<float>(0, 0, 256, 256, 256, 1, a, 256, b, 256, 0, c, 256);
     
     ASSERT_TRUE(1); // This test is here to be able to check the time it takes for different GEMM inplementations
 }
@@ -38,7 +38,7 @@ TEST(test_benchmark_gemm, benchmark_gemm_512x512_float) {
     shared_ptr<Tensor<float>> b = TensorFactory::create_tensor<float>({512, 512}, b_data);
     shared_ptr<Tensor<float>> c = TensorFactory::create_tensor<float>({512, 512});
     
-    TensorOperationsModule::gemm<float>(0, 1, 512, 512, 512, 1, a, 512, b, 512, 0, c, 512);
+    TensorOperationsModule::gemm<float>(0, 0, 512, 512, 512, 1, a, 512, b, 512, 0, c, 512);
     
     ASSERT_TRUE(1); // This test is here to be able to check the time it takes for different GEMM inplementations
 }
@@ -52,7 +52,7 @@ TEST(test_benchmark_gemm, benchmark_gemm_1024x1024_float) {
     shared_ptr<Tensor<float>> b = TensorFactory::create_tensor<float>({1024, 1024}, b_data);
     shared_ptr<Tensor<float>> c = TensorFactory::create_tensor<float>({1024, 1024});
     
-    TensorOperationsModule::gemm<float>(0, 1, 1024, 1024, 1024, 1, a, 1024, b, 1024, 0, c, 1024);
+    TensorOperationsModule::gemm<float>(0, 0, 1024, 1024, 1024, 1, a, 1024, b, 1024, 0, c, 1024);
     
     ASSERT_TRUE(1); // This test is here to be able to check the time it takes for different GEMM inplementations
 }
