43df6189e5f3010eb0a8ca61c4c1e7062f116f97
Breman402 <mans.bremer@gmail.com>
2025-04-14 14:06:25 +0200

    Add comments

--- tests/test_ImageNet.cpp
+++ tests/test_ImageNet.cpp
@@ -65,6 +65,38 @@ std::string padNumber(int num, int width = 8) {
   return ss.str();
 }
 
+/**
+ * @brief Processes a range of ImageNet images, performs inference using a pre-trained model, 
+ *        and evaluates the predictions against ground truth labels.
+ * 
+ * @param startingindex The starting index of the images to process (inclusive). Must be > 0 and <= 50000.
+ * @param endingindex The ending index of the images to process (inclusive). Must be >= startingindex and <= 50000.
+ * @return std::pair<size_t, size_t> A pair containing the number of successful predictions (first) 
+ *         and the number of failed predictions (second).
+ * 
+ * @throws std::invalid_argument If:
+ *         - endingindex < startingindex
+ *         - startingindex > 50000
+ *         - endingindex > 50000
+ *         - startingindex <= 0
+ *         - endingindex < 0
+ * 
+ * @details This function performs the following steps for each image in the specified range:
+ *          1. Loads the image from the file system.
+ *          2. Resizes and crops the image to the required dimensions.
+ *          3. Normalizes the image using predefined mean and standard deviation values.
+ *          4. Loads the image into a tensor and sets it as input for the model.
+ *          5. Runs inference using a pre-trained AlexNet model.
+ *          6. Compares the model's prediction with the ground truth label from a JSON file.
+ *          7. Tracks the number of successful and failed predictions.
+ * 
+ * @note The function assumes the existence of specific file paths for images and labels:
+ *       - Images are located in "../tests/data/imagenet/images/".
+ *       - Ground truth labels are in "../tests/data/imagenet/ILSVRC2012_validation_ground_truth.json".
+ *       - The AlexNet model is loaded from "../alexnet.json".
+ * 
+ * @warning Ensure that the file paths and required resources are correctly set up before calling this function.
+ */
 std::pair<size_t, size_t> imageNet(const size_t startingindex, const size_t endingindex) {
   if (endingindex < startingindex) {
     throw std::invalid_argument("Ending index must be larger than starting index");
