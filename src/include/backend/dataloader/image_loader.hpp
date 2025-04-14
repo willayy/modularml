2489424adc69a93beca568500a377926c1786641
Breman402 <mans.bremer@gmail.com>
2025-04-14 10:47:03 +0200

    Add load capable of loading an image from memory, instead of just from a file.

--- src/include/backend/dataloader/image_loader.hpp
+++ src/include/backend/dataloader/image_loader.hpp
@@ -10,7 +10,22 @@
  * @author Tim Carlsson (timca@chalmers.se)
  */
 class ImageLoader : public DataLoader<float> {
-public:
+ public:
+  /**
+   * @struct RawImageBuffer
+   * @brief Represents a raw image buffer containing image data and its properties.
+   *
+   * This structure is used to store the raw pixel data of an image along with
+   * its dimensions and the number of color channels.
+   *
+   */
+  struct RawImageBuffer {
+    std::shared_ptr<unsigned char> data;
+    int width;
+    int height;
+    int channels;
+  };
+
   /**
    * @brief Loads an image according to the configuration and returns a shared
    * pointer to a tensor representation of the original image.
@@ -19,4 +34,18 @@ public:
    * @return A std::shared_ptr to a Tensor containing the loaded data.
    */
   std::shared_ptr<Tensor<float>> load(const DataLoaderConfig &config) const;
+
+  /**
+   * @brief Loads a raw image buffer into a tensor representation.
+   * 
+   * This function takes a raw image buffer as input and converts it into a 
+   * tensor of type `float`. The tensor can then be used for further processing 
+   * in machine learning or other computational tasks.
+   * 
+   * @param raw The raw image buffer to be loaded. It contains the image data 
+   *            that needs to be converted into a tensor.
+   * @return A shared pointer to a `Tensor<float>` object containing the 
+   *         processed image data.
+   */
+  std::shared_ptr<Tensor<float>> load(const RawImageBuffer &raw) const;
 };
