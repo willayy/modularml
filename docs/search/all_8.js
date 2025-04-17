7242b5cb33c605d1163826c17bc884e4654989d8
Breman402 <mans.bremer@gmail.com>
2025-04-17 13:50:56 +0000

    Deploying to gh-pages from @ willayy/modularml@15863d8a79eab182e08665c5f8eaf7bd04c1b22a 🚀

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['leakyrelunode_0',['leakyrelunode',['../classLeakyReLUNode.html',1,'LeakyReLUNode'],['../classLeakyReLUNode.html#aca8db1c2ee601a12d1a15affb40c9663',1,'LeakyReLUNode::LeakyReLUNode(std::string X, std::string Y, float alpha=0.01f)'],['../classLeakyReLUNode.html#a75b60f157cff31336ec529ee1e4449c6',1,'LeakyReLUNode::LeakyReLUNode(const nlohmann::json &amp;node)']]],
-  ['load_1',['load',['../classDataLoader.html#afc99560de0d938752dbd50252c682f9b',1,'DataLoader::load()'],['../classImageLoader.html#a81091ae43c9a9590f310087f0f647fae',1,'ImageLoader::load()']]],
+  ['load_1',['load',['../classDataLoader.html#afc99560de0d938752dbd50252c682f9b',1,'DataLoader::load()'],['../classImageLoader.html#a81091ae43c9a9590f310087f0f647fae',1,'ImageLoader::load(const DataLoaderConfig &amp;config) const'],['../classImageLoader.html#a89c692507533f738ea04004853222592',1,'ImageLoader::load(const RawImageBuffer &amp;raw) const']]],
   ['logsoftmaxnode_2',['logsoftmaxnode',['../classLogSoftMaxNode.html',1,'LogSoftMaxNode'],['../classLogSoftMaxNode.html#ac4fd61714843c2539b68cf45b163e7aa',1,'LogSoftMaxNode::LogSoftMaxNode(std::string X, std::string Y, size_t axis=-1)'],['../classLogSoftMaxNode.html#afd8712ceedb33254d672a5884028bb29',1,'LogSoftMaxNode::LogSoftMaxNode(const nlohmann::json &amp;node)']]],
   ['lrnnode_5fmml_3',['lrnnode_mml',['../classLRNNode__mml.html',1,'LRNNode_mml'],['../classLRNNode__mml.html#aca91778bf629d458060d26fd7808a0b5',1,'LRNNode_mml::LRNNode_mml()']]]
 ];
--- docs/search/classes_b.js
+++ docs/search/classes_b.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['relunode_0',['ReLUNode',['../classReLUNode.html',1,'']]],
-  ['reshapenode_1',['reshapeNode',['../classreshapeNode.html',1,'']]],
-  ['resizeandcrop_2',['resizeAndCrop',['../classresizeAndCrop.html',1,'']]]
+  ['rawimagebuffer_0',['RawImageBuffer',['../structImageLoader_1_1RawImageBuffer.html',1,'ImageLoader']]],
+  ['relunode_1',['ReLUNode',['../classReLUNode.html',1,'']]],
+  ['reshapenode_2',['reshapeNode',['../classreshapeNode.html',1,'']]],
+  ['resizeandcrop_3',['resizeAndCrop',['../classresizeAndCrop.html',1,'']]]
 ];
